35dfa6de2d0909283ba2ec76544b00c9ca32feac
Tim Stoop <tim@kumina.nl>
2011-04-18 11:44:05 +0200

    Move apt preferences to kbp class. Add vim plugin class.

--- gen_puppet/manifests/init.pp
+++ gen_puppet/manifests/init.pp
@@ -1,5 +1,9 @@
 class gen_puppet {
-	gen_apt::preference { ["puppet","puppet-common"]:; }
-
 	kpackage { ["puppet","puppet-common"]:; }
+}
+
+class gen_puppet::vim {
+	kpackage { "vim-puppet":
+		ensure => latest,
+	}
 }
