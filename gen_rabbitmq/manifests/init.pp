e7260a696c619715e034c94851d94ae7e3b00ca3
Tim Stoop <tim@kumina.nl>
2011-04-18 12:03:41 +0200

    Add gen_rabbitmq.

---
+++ gen_rabbitmq/manifests/init.pp
@@ -0,0 +1,23 @@
+class gen_rabbitmq {
+	# This requires the source to be added!
+	kpackage { "rabbitmq-server":
+		ensure => latest,
+	}
+
+	exec { "reload-rabbitmq":
+		command     => "/etc/init.d/rabbitmq-server reload",
+		refreshonly => true,
+	}
+}
+
+class gen_rabbitmq::plugin::amqp {
+	include gen_rabbitmq
+
+	# This probably should be a package as well, but let's solve it with a sourced
+	# file for now.
+	kfile { "/usr/lib/rabbitmq/lib/rabbitmq_server-2.4.1/plugins/amqp_client-2.4.1.ez":
+		source  => "gen_rabbitmq/plugins/amqp_client-2.4.1.ez",
+		require => Kpackage["rabbitmq-server"],
+		notify  => Exec["reload-rabbitmq"],
+	}
+}
