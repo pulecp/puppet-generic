c69ec5e73cd155f7c2c402024673ecfa3e8c1401
Tim Stoop <tim@kumina.nl>
2011-04-18 11:46:21 +0200

    Add service for queue daemon.

--- gen_puppet/manifests/queue.pp
+++ gen_puppet/manifests/queue.pp
@@ -1,6 +1,15 @@
 class gen_puppet::queue {
+	include gen_puppet::master
+
 	# Install the stomp gem
 	kpackage { "libstomp-ruby":
 		ensure => latest,
+	}
+
+	# The service
+	service { "puppetqd":
+		hasstatus => true,
+		ensure    => running,
+		require   => Kpackage["puppetmaster"];
 	}
 }
