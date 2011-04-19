d7ae3e4e3ee52a9e3744e55119f785ae86ed2932
Rutger Spiertz <rutger@kumina.nl>
2011-04-19 17:01:08 +0200

    Fix typo

--- ferm/manifests/init.pp
+++ ferm/manifests/init.pp
@@ -44,7 +44,7 @@ class ferm::new {
 
 	define ipv4table() {
 		fermfile {
-			"${tables}['ipv4'][${name}]":
+			${tables}['ipv4']["${name}"]:
 				content => "table ${name} {";
 #			"${tables}['ipv4'][${name}]9999":
 #				content => "}";
@@ -53,8 +53,8 @@ class ferm::new {
 
 	define ipv6table() {
 		fermfile {
-			"${tables}['ipv6'][${name}]":
-				content => "domain ipv6 table ${name}";
+#			"${tables}['ipv6'][${name}]":
+#				content => "domain ipv6 table ${name}";
 #			"${tables}['ipv6'][${name}]9999":
 #				content => "}";
 		}
