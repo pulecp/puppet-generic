d80fc447194b912eb6df42baffa611b175a61cb5
Rutger Spiertz <rutger@kumina.nl>
2011-04-20 11:13:04 +0200

    Fix typo.

--- ferm/manifests/init.pp
+++ ferm/manifests/init.pp
@@ -49,7 +49,7 @@ class ferm::new {
 			notify  => Exec["reload-ferm"];
 	}
 
-	define modstate($comment="", $action=DROP, $table=filter, $chain=input) {
+	define modstate($comment="", $action=DROP, $table=filter, $chain=INPUT) {
 		$real_name = regsubst($name,'^(.*)_(.*)$','\1')
 		$ip_proto = regsubst($name,'^(.*)_(.*)$','\2')
 
