9cfe40a7f33368246cd89924048974c28a7454fe
Tim Stoop <tim@kumina.nl>
2011-04-20 16:19:25 +0200

    Puppet should control it's own file.

--- gen_puppet/manifests/init.pp
+++ gen_puppet/manifests/init.pp
@@ -1,3 +1,16 @@
 class gen_puppet {
 	kpackage { ["puppet","puppet-common"]:; }
 }
+
+class gen_puppet::puppet_conf {
+	include gen_puppet::concat
+
+	# Setup the default config file
+	concat { '/etc/puppet/puppet.conf':
+		owner => 'root',
+		group => 'root',
+		mode  => '0640',
+	}
+
+
+}
