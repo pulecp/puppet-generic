799c12c97bf97d48efc936fb061de5944a9155f1
Tim Stoop <tim@kumina.nl>
2011-04-20 16:37:55 +0200

    Add a function to set config options.

--- gen_puppet/manifests/init.pp
+++ gen_puppet/manifests/init.pp
@@ -11,6 +11,26 @@ class gen_puppet::puppet_conf {
 		group => 'root',
 		mode  => '0640',
 	}
+}
 
+define kbp_puppet::set_config ($var, $value, $configfile = '/etc/puppet/puppet.conf', $section = 'main', $order = false) {
+	# If order is set, don't use section
+	if $order {
+		$real_order = $order
+	} else {
+		# Based on section, set order
+		$real_order = $section ? {
+			'main'   => "15",
+			'agent'  => "25",
+			'master' => "35",
+			'queue'  => "45",
+			default  => fail("No order given and no known section given."),
+		}
+	}
 
+	gen_puppet::concat::add_content { $name:
+		target  => $configfile,
+		content => "${var} = ${value}",
+		order   => $real_order,
+	}
 }
