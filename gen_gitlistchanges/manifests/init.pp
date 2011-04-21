c06482eecae72aa967eefad80366ba6af99280aa
Pieter Lexis <pieter@kumina.nl>
2011-04-21 14:37:06 +0200

    Made some resources virtual so we don't duplicate them

--- gen_gitlistchanges/manifests/init.pp
+++ gen_gitlistchanges/manifests/init.pp
@@ -3,20 +3,24 @@ class gen_gitlistchanges {
         ensure => latest;
     }
 
-    define repoconfig ($to, $repo=false, $from=false, $branch=false, $since=false, $forcustomer=false) { 
-    	$the_repo = $name ? {
+    @kfile {
+        "/etc/gitlistchanges.conf":
+            content => "includedir:/etc/gitlistchanges.conf.d\n";
+        "/etc/gitlistchanges.conf.d":
+            ensure => directory;
+    }
+
+    define repoconfig ($to, $repo=false, $from=false, $branch=false, $since=false, $forcustomer=false) {
+	$the_repo = $name ? {
 		false => $repo,
 		default => $name,
 	}
 	$the_repo_safe = regsubst($the_repo, '/', '_', "G")
 
-        kfile {
-            "/etc/gitlistchanges.conf":
-                content => "includedir:/etc/gitlistchanges.conf.d\n";
-            "/etc/gitlistchanges.conf.d":
-                ensure => directory;
-            "/etc/gitlistchanges.conf.d/${the_repo_safe}-${to}":
-                content => template("gen_gitlistchanges/repoconfig.erb");
+        realize Kfile["/etc/gitlistchanges.conf", "/etc/gitlistchanges.conf.d"]
+
+        kfile { "/etc/gitlistchanges.conf.d/${the_repo_safe}-${to}":
+            content => template("gen_gitlistchanges/repoconfig.erb");
         }
     }
 }
