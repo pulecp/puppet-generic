e36aa710f6fa4e375e218834bb235b5bec29460c
Rutger Spiertz <rutger@kumina.nl>
2011-05-03 17:35:52 +0200

    Fix override syntax.

--- ferm/manifests/init.pp
+++ ferm/manifests/init.pp
@@ -18,9 +18,9 @@ class ferm {
 class ferm::release {
 	include gen_puppet::concat
 
-	kfile { "/var/lib/puppet/concat/_etc_ferm_ferm.conf_new":
+	Kfile <| title == "/var/lib/puppet/concat/_etc_ferm_ferm.conf_new" |> {
 		ensure => absent,
-		force  => true;
+		force  => true,
 	}
 
 	kpackage { "ferm":
