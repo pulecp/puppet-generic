77eba4cdfd90729a3aa8811d2507278172c262a9
Rutger Spiertz <rutger@kumina.nl>
2011-05-09 15:04:18 +0200

    Make the regexes less strict to allow for strings of IPs.

--- ferm/manifests/init.pp
+++ ferm/manifests/init.pp
@@ -82,13 +82,13 @@ class ferm::new {
 		$sanitized_name = regsubst($real_name, '[^a-zA-Z0-9\-_]', '_', 'G')
 		$ip_proto = regsubst($name,'^(.*)_(.*?)$','\2')
 		$saddr_is_ip = $saddr ? {
-			/^(! )?\d+\.\d+\.\d+\.\d+\/?\d*$/ => "ipv4",
-			/^(! )?.*:.*:.*\/?d*$/            => "ipv6",
+			/(! )?\d+\.\d+\.\d+\.\d+\/?\d*/ => "ipv4",
+			/(! )?.*:.*:.*\/?d*/            => "ipv6",
 			default                           => false,
 		}
 		$daddr_is_ip = $daddr ? {
-			/^(! )?\d+\.\d+\.\d+\.\d+\/?\d*$/ => "ipv4",
-			/^(! )?.*:.*:.*\/?d*$/            => "ipv6",
+			/(! )?\d+\.\d+\.\d+\.\d+\/?\d*/ => "ipv4",
+			/(! )?.*:.*:.*\/?d*/            => "ipv6",
 			default                           => false,
 		}
 
