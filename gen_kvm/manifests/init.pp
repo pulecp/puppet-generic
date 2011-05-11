aea22072853f86a51e815e22461824ee202c1153
Tim Stoop <tim@kumina.nl>
2011-05-11 16:40:36 +0200

    Add generic kvm package.

---
+++ gen_kvm/manifests/init.pp
@@ -0,0 +1,11 @@
+class gen_kvm {
+	# Not entirely sure what we want to do here yet. But I am sure we don't
+	# want to do anything on lenny just yet.
+	if $lsbmajdistrelease > 5 {
+		# Ensure we have the kvm setup
+		kpackage { "qemu-kvm":; }
+
+		# If things seem broke for whatever reason, install hal
+		#kpackage { "hal":; }
+	}
+}
