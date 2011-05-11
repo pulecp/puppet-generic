3fb35a4323db8636a8bb03140bbb13454c940db6
Ed Schouten <ed@kumina.nl>
2011-05-11 11:59:44 +0200

    Add gen_checkpuppet module.

---
+++ gen_checkpuppet/manifests/init.pp
@@ -0,0 +1,10 @@
+# Copyright (C) 2011 Kumina bv, Ed Schouten <ed@kumina.nl>
+# This works is published under the Creative Commons Attribution-Share
+# Alike 3.0 Unported license - http://creativecommons.org/licenses/by-sa/3.0/
+# See LICENSE for the full legal text.
+
+class gen_checkpuppet {
+	package { "checkpuppet":
+		ensure => installed;
+	}
+}
