aef5433555f9dc6723c4b0342f444f40d50cbcb1
Rutger Spiertz <rutger@kumina.nl>
2011-05-24 16:46:55 +0200

    Test new unless.

--- mysql/manifests/init.pp
+++ mysql/manifests/init.pp
@@ -137,8 +137,8 @@ class mysql::munin {
 
 define mysql::user($user, $password, $hostname="localhost") {
 	exec { "create-${user}${hostname}":
-#		unless  => "`/usr/bin/pgrep mysqld > /dev/null` && ! `/usr/bin/mysql -u ${user} -p${password} > /dev/null 2>&1`",
-		unless  => "/usr/bin/mysql -u ${user} -p${password}",
+		unless  => "`/usr/bin/pgrep mysqld > /dev/null` && ! `/usr/bin/mysql -u ${user} -p${password} > /dev/null 2>&1`",
+#		unless  => "/usr/bin/mysql -u ${user} -p${password}",
 		command => "/usr/bin/mysql --defaults-file=/etc/mysql/debian.cnf -e \"create user '${user}'@'${hostname}' identified by '${password}';\"",
 		require => Service["mysql"];
 	}
