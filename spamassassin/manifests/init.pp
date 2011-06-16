a34ba8c235bdc32b9a8327e641579e35c005bd5f
Rutger Spiertz <rutger@kumina.nl>
2011-06-16 16:10:51 +0200

    Update package and file to kpackage and kfile.

--- spamassassin/manifests/init.pp
+++ spamassassin/manifests/init.pp
@@ -1,16 +1,7 @@
 class spamassassin {
-	package { "spamassassin":
-		ensure => installed,
-	}
+	kpackage { ["spamassassin","libmail-spf-perl", "libmail-dkim-perl"]:; }
 
-	package { ["libmail-spf-perl", "libmail-dkim-perl"]:
-		ensure => installed,
-	}
-
-	file { "/etc/default/spamassassin":
-		owner => "root",
-		group => "root",
-		mode => 644,
-		source => "puppet://puppet/spamassassin/default/spamassassin",
+	kfile { "/etc/default/spamassassin":
+		source => "spamassassin/default/spamassassin";
 	}
 }
