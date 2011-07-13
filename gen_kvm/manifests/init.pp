237ac9838d56a74f6a59cd0b2ae33031ed8e5625
Rutger Spiertz <rutger@kumina.nl>
2011-07-13 14:28:24 +0200

    Update and document.

--- gen_kvm/manifests/init.pp
+++ gen_kvm/manifests/init.pp
@@ -3,20 +3,16 @@
 # Class: gen_kvm
 #
 # Actions:
-#	Undocumented
+#	Set up qemu-kvm
 #
 # Depends:
-#	Undocumented
 #	gen_puppet
 #
 class gen_kvm {
-	# Not entirely sure what we want to do here yet. But I am sure we don't
-	# want to do anything on lenny just yet.
-	if $lsbmajdistrelease > 5 {
-		# Ensure we have the kvm setup
-		kpackage { "qemu-kvm":; }
-
-		# If things seem broke for whatever reason, install hal
-		#kpackage { "hal":; }
+	# Against our policy we use a define from an other gen class here, but we'd have to do other icky stuff if we didn't
+	if $lsbmajdistrelease < 6 {
+		gen_apt::preference { "qemu-kvm":; }
 	}
+
+	kpackage { "qemu-kvm":; }
 }
