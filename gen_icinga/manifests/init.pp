411d23fa642ecc32a69dcb5786f5e4c606f26d52
Rutger Spiertz <rutger@kumina.nl>
2011-07-14 11:24:01 +0200

    Remove default from host_name in service define.

--- gen_icinga/manifests/init.pp
+++ gen_icinga/manifests/init.pp
@@ -72,7 +72,7 @@ class gen_icinga::server {
 #	name
 #		Same as Icinga
 #	host_name
-#		Same as Icinga, defaults to $fqdn
+#		Same as Icinga
 #	hostgroup_name
 #		Same as Icinga
 #	initial_state
@@ -137,7 +137,7 @@ class gen_icinga::server {
 #	gen_puppet
 #
 define gen_icinga::service($conf_dir="${environment}/${fqdn}", $use=false, $service_description=false, $servicegroups=false,
-		$host_name=$fqdn, $hostgroup_name=false, $initial_state=false, $active_checks_enabled=false, $passive_checks_enabled=false,
+		$host_name=false, $hostgroup_name=false, $initial_state=false, $active_checks_enabled=false, $passive_checks_enabled=false,
 		$obsess_over_service=false, $check_freshness=false, $freshness_threshold=false, $notifications_enabled=false, $event_handler_enabled=false, $flap_detection_enabled=false,
 		$process_perf_data=false, $retain_status_information=false, $retain_nonstatus_information=false, $notification_interval=false, $is_volatile=false, $check_period=false,
 		$check_interval=false, $retry_interval=false, $notification_period=false, $notification_options=false, $contact_groups=false, $contacts=false,
