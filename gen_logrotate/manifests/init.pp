07d1e7470d3bfac85622debf7fd9131ce7fbfbe4
Rutger Spiertz <rutger@kumina.nl>
2011-07-14 11:54:50 +0200

    Clean up and document.

--- gen_logrotate/manifests/init.pp
+++ gen_logrotate/manifests/init.pp
@@ -3,24 +3,44 @@
 # Class: gen_logrotate
 #
 # Actions:
-#	Undocumented
+#	Set up logrotate
 #
 # Depends:
-#	Undocumented
 #	gen_puppet
 #
 class gen_logrotate {
 	kpackage { "logrotate":; }
 
-	define rotate ($log, $options = [ "weekly", "compress", "rotate 7", "missingok" ], $prerotate = "NONE", $postrotate = "NONE") {
-		kfile { "/etc/logrotate.d/${name}":
-			mode    => 644,
-			content => template("gen_logrotate/logrotate.erb");
-		}
-	}
-
 	kfile { "/etc/logrotate.d/":
 		ensure => directory,
 	}
+}
 
+# Class: gen_logrotate
+#
+# Actions:
+#	Set up a logrotation
+#
+# Parameters
+#	name
+#		The name of the logrotate config file to create
+#	logs
+#		Defines which log file(s) to rotate
+#	options
+#		An array with the logrotate options, defaults to ["weekly","compress","rotate 7","missingok"]
+#	prerotate
+#		Defines a command to run before rotating the log
+#	postrotate
+#		Defines a command to run after rotating the log
+#
+# Depends:
+#	gen_logrotate
+#	gen_puppet
+#
+define gen_logrotate::rotate ($logs, $options=["weekly","compress","rotate 7","missingok"], $prerotate=false, $postrotate=false) {
+	include gen_logrotate
+
+	kfile { "/etc/logrotate.d/${name}":
+		content => template("gen_logrotate/logrotate.erb");
+	}
 }
