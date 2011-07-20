271075f1a65b9d5bd5e5cd1f191b0f5e2d967770
Rutger Spiertz <rutger@kumina.nl>
2011-07-20 16:37:34 +0200

    Fix monitoring fact script.

--- gen_puppet/lib/facter/monitoring.rb
+++ gen_puppet/lib/facter/monitoring.rb
@@ -1,12 +1,12 @@
-File.exist?("/etc/puppet/nomonitoring") ? has_file = false : has_file = true
+File.exist?("/etc/puppet/nomonitoring") ? has_monitoringfile = false : has_monitoringfile = true
 Facter.add("monitoring") do
 	setcode do
-		has_file
+		has_monitoringfile
 	end
 end
-File.exist?("/etc/puppet/nomonitoringsms") ? has_file = false : has_file = true
+File.exist?("/etc/puppet/nomonitoringsms") ? has_monitoringsmsfile = false : has_monitoringsmsfile = true
 Facter.add("monitoring_sms") do
 	setcode do
-		has_file
+		has_monitoringsmsfile
 	end
 end
