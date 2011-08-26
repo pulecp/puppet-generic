b7622ed89d87740f11424bd2e70b83fff0e70cab
Pieter Lexis <pieter@kumina.nl>
2011-08-26 10:18:55 +0200

    remove legacy facts

--- common/lib/facter/augeas_version.rb
+++
@@ -1,7 +0,0 @@
-if  FileTest.exists?("/usr/bin/dpkg-query")
-	Facter.add("augeas_version") do
-		setcode do
-			%x{/usr/bin/dpkg-query -W -f='${Version}' augeas-lenses}
-		end
-	end
-end
--- common/lib/facter/debug_manifest.rb
+++
@@ -1,6 +0,0 @@
-File.exist?("/etc/puppet/debug_manifest") ? has_file = true : has_file = false
-Facter.add("debug_manifest") do
-	setcode do
-		has_file
-	end
-end
