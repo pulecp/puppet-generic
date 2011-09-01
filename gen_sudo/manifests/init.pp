d078646b7247fed4298f6926c16b5dffc272758c
Rutger Spiertz <rutger@kumina.nl>
2011-09-01 15:04:44 +0200

    Update and document gen_sudo.

--- gen_sudo/manifests/init.pp
+++ gen_sudo/manifests/init.pp
@@ -3,66 +3,92 @@
 # Class: gen_sudo
 #
 # Actions:
-#	Undocumented
+#	Install sudo and set up basics
 #
 # Depends:
-#	Undocumented
 #	gen_puppet
 #
 class gen_sudo {
 	kpackage { "sudo":; }
+
 	# Setup /etc/sudoers for either .d inclusion or concatination
-	if $lsbmajdistrelease > 5 { # Squeeze and newer
-		kfile { "/etc/sudoers.d/":
-			ensure  => directory,
-			source  => "gen_sudo/sudoers.d/",
-			recurse => true,
-			purge   => true,
+	if $lsbmajdistrelease < 6 { # Lenny and older
+		concat { "/etc/sudoers":
+			mode    => 440,
+			require => Package["sudo"];
+		}
+	} else { # Squeeze and newer
+		kfile {
+			"/etc/sudoers.d/":
+				ensure  => directory,
+				recurse => true,
+				purge   => true,
+				mode    => 440;
+			"/etc/sudoers":
+				content => "#includedir /etc/sudoers.d\n",
+				mode    => 440,
+				require => Package["sudo"];
+		}
+	}
+}
+
+# Define: gen_sudo::rule
+#
+# Actions:
+#	Set up a sudo rule
+#
+# Parameters:
+#	entity
+#		The user or group that can use the rule
+#	command
+#		The command that can be run
+#	as_user
+#		The user the command can be ran as
+#	password_required
+#		Is entering a password required? Defaults to true
+#	comment
+#		Optional comment, if none is supplied the resource name will be used
+#	preserve_env_vars
+#		Do the environment vars need to be preserved? Defaults to false
+#
+# Depends:
+#	gen_puppet
+#
+define gen_sudo::rule($entity, $command, $as_user, $password_required = true, $comment = false, $preserve_env_vars=false) {
+	include gen_sudo
+
+	$the_comment = $comment ? {
+		false   => $name,
+		default => $comment
+	}
+
+	if $lsbmajdistrelease > 5 { # Squeeze or newer
+		kfile { "/etc/sudoers.d/${name}":
+			content => template("gen_sudo/sudo"),
 			mode    => 440;
 		}
-
-		kfile { "/etc/sudoers":
-			content => "#includedir /etc/sudoers.d\n",
-			owner   => "root",
-			group   => "root",
-			mode    => 440,
-			require => Package["sudo"];
-		}
-	} else { # Lenny and older
-		$sudoers="/etc/sudoers" #in squeeze or higher we could use /etc/sudoers.d/ and put all the sudo config there.
-
-		concat { $sudoers:
-			owner   => "root",
-			group   => "root",
-			mode    => 440,
-			require => Package["sudo"];
+	} else {
+		gen_sudo::add_rule { "${name}":
+			content => template("gen_sudo/sudo");
 		}
 	}
+}
 
-	# This define is internal to gen_sudo and only needed on Lenny and older hosts
-	define add_rule($content, $order=15) {
-		concat::add_content { $name:
-			content => $content,
-			order   => $order,
-			target  => "/etc/sudoers";
-		}
-	}
-
-	define rule($entity, $command, $as_user, $password_required = true, $comment = false, $order = 15, $preserve_env_vars=false) {
-		$the_comment = $comment ? {
-			false   => $name,
-			default => $comment
-		}
-		if $lsbmajdistrelease > 5 { # Squeeze or newer
-			kfile { "/etc/sudoers.d/${name}":
-				mode => 440,
-				content => template("gen_sudo/sudo.erb"),
-			}
-		} else {
-			add_rule { "${name}":
-				content => template("gen_sudo/sudo.erb"),
-				order   => $order;
-			}
-		}
+# Define: gen_sudo::add_rule
+#
+# Actions:
+#	Internal define to add a sudo rule
+#
+# Parameters:
+#	content
+#		The content of the rule
+#
+# Depends:
+#	gen_puppet
+#
+define gen_sudo::add_rule($content) {
+	concat::add_content { $name:
+		content => $content,
+		target  => "/etc/sudoers";
 	}
 }
