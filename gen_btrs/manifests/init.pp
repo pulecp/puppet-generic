b93b63c0a935298616cf8f36a3c7f7021a6ce92c
Tim Stoop <tim@kumina.nl>
2011-11-04 15:49:48 +0100

    Add some documentation.

--- gen_btrs/manifests/init.pp
+++ gen_btrs/manifests/init.pp
@@ -1,3 +1,13 @@
+# Author: Kumina bv <support@kumina.nl>
+
+# Class: gen_btrfs
+#
+# Actions:
+#  Install the btrfs tools.
+#
+# Depends:
+#  gen_puppet
+#
 class gen_btrfs {
   kpackage { "btrfs-tools":
     ensure => latest,
--- gen_ceph/manifests/init.pp
+++ gen_ceph/manifests/init.pp
@@ -1,3 +1,14 @@
+# Author: Kumina bv <support@kumina.nl>
+
+# Class: gen_ceph
+#
+# Actions:
+#  Install the ceph package. Which makes this currently an unusable class,
+#  since you need extra sources for even finding the package.
+#
+# Depends:
+#  gen_puppet
+#
 class gen_ceph {
   kpackage { "ceph":
     ensure => latest,
