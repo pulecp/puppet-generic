43b65a276fcab8b61dd36de8dce3f931b33305e6
Tim Stoop <tim@kumina.nl>
2011-11-07 16:18:42 +0100

    Replace tabs with spaces.

--- ksplice/manifests/init.pp
+++ ksplice/manifests/init.pp
@@ -8,51 +8,51 @@
 # Class: ksplice
 #
 # Actions:
-#	Undocumented
+#  Undocumented
 #
 # Depends:
-#	Undocumented
-#	gen_puppet
+#  Undocumented
+#  gen_puppet
 #
 class ksplice {
-	# Add the source repo
-	gen_apt::source { "ksplice":
-		uri          => "http://www.ksplice.com/apt",
-		distribution => "${lsbdistcodename}",
-		components   => ["ksplice"],
-	}
+  # Add the source repo
+  gen_apt::source { "ksplice":
+    uri          => "http://www.ksplice.com/apt",
+    distribution => "${lsbdistcodename}",
+    components   => ["ksplice"],
+  }
 
-	# Preseed the ksplice package
-	kfile { "/var/cache/debconf/ksplice.preseed":
-		source => "ksplice/ksplice.preseed";
-	}
+  # Preseed the ksplice package
+  kfile { "/var/cache/debconf/ksplice.preseed":
+    source => "ksplice/ksplice.preseed";
+  }
 
-	# Install the ksplice package
-	kpackage { "uptrack":
-		ensure       => latest,
-		responsefile => "/var/cache/debconf/ksplice.preseed",
-		require      => File["/var/cache/debconf/ksplice.preseed"],
-		notify       => Exec["initial uptrack run"];
-	}
+  # Install the ksplice package
+  kpackage { "uptrack":
+    ensure       => latest,
+    responsefile => "/var/cache/debconf/ksplice.preseed",
+    require      => File["/var/cache/debconf/ksplice.preseed"],
+    notify       => Exec["initial uptrack run"];
+  }
 
-	# Install the ksplice additional apps (includes nagios plugins)
-	kpackage { "python-ksplice-uptrack":; }
+  # Install the ksplice additional apps (includes nagios plugins)
+  kpackage { "python-ksplice-uptrack":; }
 
-	# Run the script when it's first installed
-	exec { "initial uptrack run":
-		command     => "/usr/sbin/uptrack-upgrade -y; exit 0",
-		refreshonly => true,
-		require     => File["/etc/uptrack/uptrack.conf"],
-	}
+  # Run the script when it's first installed
+  exec { "initial uptrack run":
+    command     => "/usr/sbin/uptrack-upgrade -y; exit 0",
+    refreshonly => true,
+    require     => File["/etc/uptrack/uptrack.conf"],
+  }
 
-	# The modified configuration file
-	kfile { "/etc/uptrack/uptrack.conf":
-		source  => "ksplice/uptrack.conf",
-		require => Package["uptrack"],
-	}
+  # The modified configuration file
+  kfile { "/etc/uptrack/uptrack.conf":
+    source  => "ksplice/uptrack.conf",
+    require => Package["uptrack"],
+  }
 
-	# Set directory permissions so Nagios can read status
-	kfile { "/var/cache/uptrack":
-		require => Package["uptrack"];
-	}
+  # Set directory permissions so Nagios can read status
+  kfile { "/var/cache/uptrack":
+    require => Package["uptrack"];
+  }
 }
