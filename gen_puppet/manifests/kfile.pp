308e41d9a8bd8f94910eb4496ba4635fee13a0c1
Rutger Spiertz <rutger@kumina.nl>
2011-11-28 16:21:18 +0100

    Add false to undef conversion to kfile for owner, group and mode.

--- gen_puppet/manifests/kfile.pp
+++ gen_puppet/manifests/kfile.pp
@@ -3,82 +3,91 @@
 # Define: kfile
 #
 # Parameters:
-#	mode
-#		Undocumented
-#	content
-#		Undocumented
-#	recurse
-#		Undocumented
-#	source
-#		Undocumented
-#	path
-#		Undocumented
-#	target
-#		Undocumented
-#	force
-#		Undocumented
-#	owner
-#		Undocumented
-#	purge
-#		Undocumented
-#	group
-#		Undocumented
-#	ignore
-#		Undocumented
-#	ensure
-#		Undocumented
+#  mode
+#    Undocumented
+#  content
+#    Undocumented
+#  recurse
+#    Undocumented
+#  source
+#    Undocumented
+#  path
+#    Undocumented
+#  target
+#    Undocumented
+#  force
+#    Undocumented
+#  owner
+#    Undocumented
+#  purge
+#    Undocumented
+#  group
+#    Undocumented
+#  ignore
+#    Undocumented
+#  ensure
+#    Undocumented
 #
 # Actions:
-#	Undocumented
+#  Undocumented
 #
 # Depends:
-#	Undocumented
-#	gen_puppet
+#  Undocumented
+#  gen_puppet
 #
-define kfile ($ensure="present", $content=false, $source=false, $path=false, $target=false, $owner="root", $group="root", $mode="0644", $recurse=false, $replace=true, $force=false, $purge=false, $ignore=false) {
-	file { "${name}":
-		ensure  => $ensure,
-		content => $content ? {
-			false   => undef,
-			default => $content,
-		},
-		source  => $source ? {
-			false   => undef,
-			default => "puppet:///modules/${source}",
-		},
-		path    => $path ? {
-			false   => undef,
-			default => $path,
-		},
-		target  => $target ? {
-			false   => undef,
-			default => $target,
-		},
-		owner   => $owner,
-		group   => $group,
-		mode    => $ensure ? {
-			directory => $mode ? {
-				"0644"   => "0755",
-				default =>  $mode,
-			},
-			default   => $mode,
-		},
-		recurse => $recurse ? {
-			false   => undef,
-			default => $recurse,
-		},
-		replace => $replace,
-		force   => $force ? {
-			false   => undef,
-			default => $force,
-		},
-		purge   => $purge ? {
-			false   => undef,
-			default => $purge,
-		},
-		ignore  => $ignore ? {
-			false   => undef,
-			default => $ignore,
-		};
-	}
+define kfile ($ensure="present", $content=false, $source=false, $path=false, $target=false, $owner="root",
+      $group="root", $mode="0644", $recurse=false, $replace=true, $force=false, $purge=false, $ignore=false) {
+  file { $name:
+    ensure  => $ensure,
+    content => $content ? {
+      false   => undef,
+      default => $content,
+    },
+    source  => $source ? {
+      false   => undef,
+      default => "puppet:///modules/${source}",
+    },
+    path    => $path ? {
+      false   => undef,
+      default => $path,
+    },
+    target  => $target ? {
+      false   => undef,
+      default => $target,
+    },
+    owner   => $owner ? {
+      false   => undef,
+      default => $owner,
+    },
+    group   => $group ? {
+      false   => undef,
+      default => $group,
+    },
+    mode    => $ensure ? {
+      directory => $mode ? {
+        false   => undef,
+        "0644"  => "0755",
+        default =>  $mode,
+      },
+      false     => undef,
+      default   => $mode,
+    },
+    recurse => $recurse ? {
+      false   => undef,
+      default => $recurse,
+    },
+    replace => $replace,
+    force   => $force ? {
+      false   => undef,
+      default => $force,
+    },
+    purge   => $purge ? {
+      false   => undef,
+      default => $purge,
+    },
+    ignore  => $ignore ? {
+      false   => undef,
+      default => $ignore,
+    };
+  }
 }
