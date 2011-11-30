bb18a97b7865367b3368d76f0c362619ad8ec3a6
Rutger Spiertz <rutger@kumina.nl>
2011-11-30 09:12:15 +0100

    Consolidate define into class.

--- gen_heartbeat/manifests/init.pp
+++ gen_heartbeat/manifests/init.pp
@@ -8,7 +8,8 @@
 # Depends:
 #  gen_puppet
 #
-class gen_heartbeat ($customtag="heartbeat_${environment}") {
+class gen_heartbeat ($autojoin="none", $warntime=5, $deadtime=15, $initdead=60, $keepalive=2, $crm="respawn",
+      $node_name=$hostname, $node_dev="eth0", $node_ip=$ipaddress_eth0, $heartbeat_tag="heartbeat_${environment}") {
   kservice { "heartbeat":; }
 
   concat { "/etc/heartbeat/ha.cf":
@@ -16,55 +17,23 @@ class gen_heartbeat ($customtag="heartbeat_${environment}") {
     notify  => Service["heartbeat"];
   }
 
-  Concat::Add_content <<| tag == $customtag |>>
+  concat::add_content { "default heartbeat config":
+    content => template("gen_heartbeat/ha.cf.erb"),
+    target  => "/etc/heartbeat/ha.cf";
+  }
+
+  @@concat::add_content { "heartbeat node ${node_name}":
+    content => "node ${node_name}\nucast ${node_dev} ${node_ip}",
+    target  => "/etc/heartbeat/ha.cf",
+    tag     => $heartbeat_tag;
+  }
+
+  Concat::Add_content <<| tag == $heartbeat_tag |>>
 
   # We don't use auth-keys, as the port is firewalled and only open to the other hosts(s) in the cluster(done in kbp_heartbeat)
   kfile { "/etc/ha.d/authkeys":
     content => "auth 1\n1 crc",
     mode    => 600,
     require => Package["heartbeat"];
-  }
-}
-
-# Define: gen_heartbeat::ha_cf
-#
-# Actions:
-#  Insert some fragments for the ha.cf file for heartbeat
-#
-#  Parameters
-#    autojoin
-#      see man 5 ha.cf
-#    warntime
-#      see man 5 ha.cf
-#    deadtime
-#      see man 5 ha.cf
-#    initdead
-#      see man 5 ha.cf
-#    keepalive
-#      see man 5 ha.cf
-#    crm
-#      see man 5 ha.cf
-#    node_name
-#      The name of the node(this is used to build the node directives in ha.cf)
-#    node_dev
-#      The device used for heartbeat communication
-#    node_ip
-#      The IP used 
-#    customtag
-#      Used when exporting and importing the configuration options. Change this when you have more than 1 heartbeat cluster.
-#
-# Depends:
-#  gen_puppet
-#
-define gen_heartbeat::ha_cf ($autojoin="none", $warntime=5, $deadtime=15, $initdead=60, $keepalive=2, $crm="respawn", $node_name=$hostname, $node_dev="eth0", $node_ip=$ipaddress_eth0, $customtag="heartbeat_${environment}") {
-  concat::add_content { "default heartbeat config":
-    content    => template("gen_heartbeat/ha.cf.erb"),
-    target     => "/etc/heartbeat/ha.cf";
-  }
-
-  @@concat::add_content { "heartbeat node ${node_name}":
-    content => "node ${node_name}\nucast ${node_dev} ${node_ip}",
-    target  => "/etc/heartbeat/ha.cf",
-    tag     => $customtag;
   }
 }
