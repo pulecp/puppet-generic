4fe45baa44f01edfae7622c2835d60206847537c
Thomas Ronner <thomas@kumina.nl>
2011-12-05 15:29:46 +0100

    HostHenker: allow correct lines to exist anywhere in the /etc/hosts file instead of only the last.

--- hosthenker/files/hosthenker.sh
+++ hosthenker/files/hosthenker.sh
@@ -12,6 +12,7 @@ HOSTNAME="$1"
 PORT="$2"
 HOST1="$3"
 HOST2="$4"
+PID=$$
 
 if nc -zw 15 "$HOST1" "$PORT"
 then
@@ -20,10 +21,11 @@ else
 	ADDR="$HOST2"
 fi
 
-egrep -v "\b$HOSTNAME\b" /etc/hosts > /etc/hosts.new
-echo "$ADDR $HOSTNAME" >> /etc/hosts.new
-if ! cmp -s /etc/hosts /etc/hosts.new
+grep -q "^$ADDR $HOSTNAME$" /etc/hosts && exit 0
+egrep -v "\b$HOSTNAME\b" /etc/hosts > /etc/hosts.new.$PID
+echo "$ADDR $HOSTNAME" >> /etc/hosts.new.$PID
+if ! cmp -s /etc/hosts /etc/hosts.new.$PID
 then
 	echo "$0: $HOSTNAME: updating record to address $ADDR" >&2
 fi
-mv /etc/hosts.new /etc/hosts
+mv /etc/hosts.new.$PID /etc/hosts
