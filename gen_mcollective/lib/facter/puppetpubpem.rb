f1b4055b6899823aad200d6f2efb45ae2d95800f
Pieter Lexis <pieter@kumina.nl>
2011-12-13 09:40:41 +0100

    Fix errors in /puppetpubpem.rb on first run.

--- gen_mcollective/lib/facter/puppetpubpem.rb
+++ gen_mcollective/lib/facter/puppetpubpem.rb
@@ -1,13 +1,12 @@
 # puppetpubpem.rb
 
-Facter.add("puppetpubpem") do
-	setcode do
-		value = nil
-		if FileTest.file?(["/var/lib/puppet/ssl/public_keys/",Facter.value(:fqdn),".pem"].join)
-			File.open(["/var/lib/puppet/ssl/public_keys/",Facter.value(:fqdn),".pem"].join) { |f|
-				value = f.read
+file = "/var/lib/puppet/ssl/public_keys/#{Facter.value(:fqdn)}.pem"
+if FileTest.file?(file)
+	Facter.add("puppetpubpem") do
+		setcode do
+			File.open(file) { |f|
+				f.read.gsub!(/\n/, ";")
 			}
 		end
-		value.gsub!(/\n/, ";")
 	end
 end
