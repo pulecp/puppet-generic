61fe935944584be8798198f01c9bdd457a066170
Rutger Spiertz <rutger@kumina.nl>
2011-12-16 10:29:03 +0100

    Switch vars to globals.

--- gen_icinga/manifests/init.pp
+++ gen_icinga/manifests/init.pp
@@ -148,7 +148,7 @@ define gen_icinga::service($conf_dir="${environment}/${fqdn}", $use=false, $serv
     $check_interval=false, $retry_interval=false, $notification_period=false, $notification_options=false, $contact_groups=false, $contacts=false,
     $max_check_attempts=false, $check_command=false, $arguments=false, $register=false, $nrpe=false, $ensure=present,
     $proxy=false) {
-  if ${::monitoring} == "true" {
+  if $::monitoring == "true" {
     @@ekfile { "/etc/icinga/config/${conf_dir}/service_${name}.cfg;${::fqdn}":
       content => template("gen_icinga/service"),
       notify  => Exec["reload-icinga"],
@@ -218,7 +218,7 @@ define gen_icinga::host($conf_dir="${environment}/${fqdn}", $use=false, $hostgro
     $notifications_enabled=false, $event_handler_enabled=false, $flap_detection_enabled=false, $process_perf_data=false, $retain_status_information=false, $retain_nonstatus_information=false,
     $check_command="check-host-alive", $check_interval=false, $notification_period=false, $notification_interval=false, $contact_groups=false, $contacts=false,
     $max_check_attempts=false, $register=false, $proxy=false) {
-  if ${::monitoring} == "true" {
+  if $::monitoring == "true" {
     @@ekfile { "/etc/icinga/config/${conf_dir}/host_${name}.cfg;${fqdn}":
       ensure  => $ensure,
       content => template("gen_icinga/host"),
@@ -249,7 +249,7 @@ define gen_icinga::host($conf_dir="${environment}/${fqdn}", $use=false, $hostgro
 #  gen_puppet
 #
 define gen_icinga::hostgroup($hg_alias, $conf_dir="${environment}/${fqdn}", $members=false) {
-  if ${::monitoring} == "true" {
+  if $::monitoring == "true" {
     @@ekfile { "/etc/icinga/config/${conf_dir}/hostgroup_${name}.cfg;${fqdn}":
       content => template("gen_icinga/hostgroup"),
       notify  => Exec["reload-icinga"],
@@ -277,7 +277,7 @@ define gen_icinga::hostgroup($hg_alias, $conf_dir="${environment}/${fqdn}", $mem
 #  gen_puppet
 #
 define gen_icinga::servicegroup($sg_alias, $conf_dir="${environment}/${fqdn}") {
-  if ${::monitoring} == "true" {
+  if $::monitoring == "true" {
     @@ekfile { "/etc/icinga/config/${conf_dir}/servicegroup_${name}.cfg;${fqdn}":
       content => template("gen_icinga/servicegroup"),
       notify  => Exec["reload-icinga"],
@@ -305,7 +305,7 @@ define gen_icinga::servicegroup($sg_alias, $conf_dir="${environment}/${fqdn}") {
 #  gen_puppet
 #
 define gen_icinga::contactgroup($cg_alias, $conf_dir="${environment}/${fqdn}") {
-  if ${::monitoring} == "true" {
+  if $::monitoring == "true" {
     @@ekfile { "/etc/icinga/config/${conf_dir}/contactgroup_${name}.cfg;${fqdn}":
       content => template("gen_icinga/contactgroup"),
       notify  => Exec["reload-icinga"],
@@ -350,7 +350,7 @@ define gen_icinga::contact($c_alias, $contact_data=false, $notification_type=fal
     },
   }
 
-  if ${::monitoring} == "true" {
+  if $::monitoring == "true" {
     @@ekfile { "/etc/icinga/config/${conf_dir}/contact_${name}.cfg;${fqdn}":
       ensure  => $ensure,
       content => template("gen_icinga/contact"),
@@ -393,7 +393,7 @@ define gen_icinga::contact($c_alias, $contact_data=false, $notification_type=fal
 #  gen_puppet
 #
 define gen_icinga::timeperiod($tp_alias, $conf_dir="${environment}/${fqdn}", $monday=false, $tuesday=false, $wednesday=false, $thursday=false, $friday=false, $saturday=false, $sunday=false) {
-  if ${::monitoring} == "true" {
+  if $::monitoring == "true" {
     @@ekfile { "/etc/icinga/config/${conf_dir}/timeperiod_${name}.cfg;${fqdn}":
       content => template("gen_icinga/timeperiod"),
       notify  => Exec["reload-icinga"],
@@ -419,7 +419,7 @@ define gen_icinga::timeperiod($tp_alias, $conf_dir="${environment}/${fqdn}", $mo
 #  gen_puppet
 #
 define gen_icinga::configdir($ensure="present",$base="/etc/icinga/config") {
-  if ${::monitoring} == "true" {
+  if $::monitoring == "true" {
     @@ekfile { "${base}/${name};${fqdn}":
       ensure => $ensure ? {
         "present" => "directory",
@@ -458,7 +458,7 @@ define gen_icinga::configdir($ensure="present",$base="/etc/icinga/config") {
 #  gen_puppet
 #
 define gen_icinga::servercommand($conf_dir="${environment}/${fqdn}", $command_name=false, $host_argument='-H $HOSTADDRESS$', $arguments=false, $nrpe=false, $time_out=30) {
-  if ${::monitoring} == "true" {
+  if $::monitoring == "true" {
     @@ekfile {
       "/etc/icinga/config/${conf_dir}/command_${name}.cfg;${fqdn}":
         content => template("gen_icinga/command"),
@@ -507,7 +507,7 @@ define gen_icinga::servercommand($conf_dir="${environment}/${fqdn}", $command_na
 #  gen_puppet
 #
 define gen_icinga::hostescalation($escalation_period, $contact_groups="${environment}/${fqdn}", $contacts=false, $conf_dir=false, $host_name=false, $hostgroup_name=false, $escalation_options=false, $first_notification=1, $last_notification=0, $notification_interval=0) {
-  if ${::monitoring} == "true" {
+  if $::monitoring == "true" {
     @@ekfile { "/etc/icinga/config/${conf_dir}/host_escalation_${name}.cfg;${fqdn}":
       content => template("gen_icinga/hostescalation"),
       notify  => Exec["reload-icinga"],
@@ -555,7 +555,7 @@ define gen_icinga::hostescalation($escalation_period, $contact_groups="${environ
 #  gen_puppet
 #
 define gen_icinga::serviceescalation($escalation_period, $contact_groups=false, $contacts=false, $conf_dir="${environment}/${fqdn}", $host_name=false, $hostgroup_name=false, $servicegroup_name=false, $service_description="*", $escalation_options=false, $first_notification=1, $last_notification=0, $notification_interval=0) {
-  if ${::monitoring} == "true" {
+  if $::monitoring == "true" {
     @@ekfile { "/etc/icinga/config/${conf_dir}/service_escalation_${name}.cfg;${fqdn}":
       content => template("gen_icinga/serviceescalation"),
       notify  => Exec["reload-icinga"],
@@ -595,7 +595,7 @@ define gen_icinga::serviceescalation($escalation_period, $contact_groups=false, 
 #  gen_puppet
 #
 define gen_icinga::servicedependency($dependent_service_description, $host_name, $service_description, $conf_dir="${environment}/${fqdn}", $dependent_host_name=$fqdn, $execution_failure_criteria=false, $notification_failure_criteria="o") {
-  if ${::monitoring} == "true" {
+  if $::monitoring == "true" {
     @@ekfile { "/etc/icinga/config/${conf_dir}/service_dependency_${name}.cfg;${fqdn}":
       content => template("gen_icinga/servicedependency"),
       notify  => Exec["reload-icinga"],
