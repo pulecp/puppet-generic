e21ba17bb9de62a3a7b4164d5e622d4c253742b6
Rutger Spiertz <rutger@kumina.nl>
2011-12-20 14:19:01 +0100

    Make sure the backup dir exists.

--- localbackup/manifests/init.pp
+++ localbackup/manifests/init.pp
@@ -3,62 +3,66 @@
 # Class: localbackup::common
 #
 # Actions:
-#	Undocumented
+#  Undocumented
 #
 # Depends:
-#	Undocumented
-#	gen_puppet
+#  Undocumented
+#  gen_puppet
 #
 class localbackup::common {
-	define line($file, $line, $ensure = 'present') {
-		case $ensure {
-			default: {
-				err("unknown ensure value ${ensure}")
-			}
-			present: {
-				exec { "/bin/echo '${line}' >> '${file}'":
-					unless => "/bin/grep -Fx '${line}' '${file}'"
-				}
-			}
-			absent: {
-				exec { "/usr/bin/perl -ni -e 'print unless /^\\Q${line}\\E\$/' '${file}'":
-					onlyif => "/bin/grep -Fx '${line}' '${file}'"
-				}
-			}
-		}
-	}
+  define line($file, $line, $ensure = 'present') {
+    case $ensure {
+      default: {
+        err("unknown ensure value ${ensure}")
+      }
+      present: {
+        exec { "/bin/echo '${line}' >> '${file}'":
+          unless => "/bin/grep -Fx '${line}' '${file}'"
+        }
+      }
+      absent: {
+        exec { "/usr/bin/perl -ni -e 'print unless /^\\Q${line}\\E\$/' '${file}'":
+          onlyif => "/bin/grep -Fx '${line}' '${file}'"
+        }
+      }
+    }
+  }
 }
 
 # Class: localbackup::client
 #
 # Actions:
-#	Undocumented
+#  Undocumented
 #
 # Depends:
-#	Undocumented
-#	gen_puppet
+#  Undocumented
+#  gen_puppet
 #
 class localbackup::client {
   include gen_base::backup-scripts
 
-	package { "local-backup":
-		ensure => latest;
-	}
+  package { "local-backup":
+    ensure => latest;
+  }
 
-	file { "/etc/backup/local-backup.conf":
-		content => template("localbackup/local-backup.conf"),
-		require => Package["local-backup"];
-	}
+  kfile { "/backup":
+    ensure => directory;
+  }
 
-	file { "/etc/backup/prepare.d/dpkg-list":
-		ensure => symlink,
-		target => "/usr/share/local-backup/prepare/dpkg-list",
-		require => Package["local-backup"];
-	}
+  file { "/etc/backup/local-backup.conf":
+    content => template("localbackup/local-backup.conf"),
+    require => Package["local-backup"];
+  }
 
-	file { "/etc/backup/prepare.d/filesystems":
-		ensure => symlink,
-		target => "/usr/share/local-backup/prepare/filesystems",
-		require => Package["local-backup"];
-	}
+  file { "/etc/backup/prepare.d/dpkg-list":
+    ensure => symlink,
+    target => "/usr/share/local-backup/prepare/dpkg-list",
+    require => Package["local-backup"];
+  }
+
+  file { "/etc/backup/prepare.d/filesystems":
+    ensure => symlink,
+    target => "/usr/share/local-backup/prepare/filesystems",
+    require => Package["local-backup"];
+  }
 }
