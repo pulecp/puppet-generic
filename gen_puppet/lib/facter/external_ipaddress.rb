5a0bbddf3b1da655cf9b864bc4f015970f93fe84
Tim Stoop <tim@kumina.nl>
2011-12-29 10:57:45 +0100

    Rewrite the creation of the fact, the separate method doesn't seem to work as expected.

--- gen_puppet/lib/facter/external_ipaddress.rb
+++ gen_puppet/lib/facter/external_ipaddress.rb
@@ -1,15 +1,19 @@
-def add_fact(code)
-  Facter.add("external_ipaddress") { setcode { code } }
-end
-
-# Do we have a fact called ec2_public_ipv4?
-add_fact(Facter.value(:ec2_public_ipv4)) if ! Facter.value(:ec2_public_ipv4).nil?
-
 begin
-  # Get the route to 8.8.8.8, get the ip address mentioned after 'src', this is the IP that has the source address (and is most likely the external IP).
-  ip = %x{/bin/ip route get 8.8.8.8}.grep(/src/)[0].split.last
+  # Do we have a fact called ec2_public_ipv4?
+  if Facter.value(:ec2_public_ipv4).nil?
+    # Get the route to 8.8.8.8, get the ip address mentioned after 'src', this is the IP that has the source address (and is most likely the external IP).
+    ip = %x{/bin/ip route get 8.8.8.8}.grep(/src/)[0].split.last
+  else
+    ip = Facter.value(:ec2_public_ipv4)
+  end
   # Add the fact if the found IP is not a local loopback address or an RFC 1918 address.
-  add_fact(ip) if ip.grep(/(^127\.0\.0\.1)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)/).empty?
+  if ip.grep(/(^127\.0\.0\.1)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)/).empty?
+    Facter.add("external_ipaddress") do
+      setcode do
+        ip
+      end
+    end
+  end
 rescue
 # do nothing
 end
