f08084d63f60b2d646f81f0fcec72e83d265a94f
Thomas Ronner <thomas@kumina.nl>
2012-01-11 11:33:02 +0100

    Fix gen_base::krb5 for lenny.

--- gen_base/manifests/init.pp
+++ gen_base/manifests/init.pp
@@ -220,8 +220,10 @@ class gen_base::jmxquery {
 #  gen_puppet
 #
 class gen_base::krb5 {
-  kpackage { ["libgssapi-krb5-2","libk5crypto3","libkrb5-3","libkrb5support0"]:
-    ensure => latest;
+  if $lsbmajdistrelease == 6 {
+    kpackage { ["libgssapi-krb5-2","libk5crypto3","libkrb5-3","libkrb5support0"]:
+      ensure => latest;
+    }
   }
 }
 
