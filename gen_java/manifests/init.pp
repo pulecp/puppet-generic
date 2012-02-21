fe1748429dbe62c23e67c75385eb90f214bbe44f
Pieter Lexis <pieter@kumina.nl>
2012-02-21 16:45:54 +0100

    Add java crypto libraries

--- gen_java/manifests/init.pp
+++ gen_java/manifests/init.pp
@@ -8,6 +8,22 @@ class gen_java::sun_java6_jdk {
   }
 }
 
+# Class: gen_java::sun_java6_jre_crypto_policy
+#
+# Action:
+#  Install strong crypto libraries for java
+#
+class gen_java::sun_java6_jre_crypto_policy {
+  kfile {
+    "/usr/lib/jvm/java-6-sun/jre/lib/security/US_export_policy.jar":
+      source  => "gen_java/jce/US_export_policy.jar",
+      require => Package["sun-java6-jre"];
+    "/usr/lib/jvm/java-6-sun/jre/lib/security/local_policy.jar":
+      source => "gen_java/jce/local_policy.jar",
+      require => Package["sun-java6-jre"];
+  }
+}
+
 class gen_java::sun_java6_jre {
 
   kfile { '/tmp/sunlicense.preseed':
