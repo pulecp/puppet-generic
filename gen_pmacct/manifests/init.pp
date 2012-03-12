a9074fe860c7ae8a693dd19ee28de2bd4b2f2d4a
Tim Stoop <tim@kumina.nl>
2012-03-12 11:43:32 +0100

    Make sure it starts the correct config. Add some additional optimizations.

--- gen_pmacct/manifests/init.pp
+++ gen_pmacct/manifests/init.pp
@@ -20,7 +20,7 @@ class gen_pmacct {
 #
 # Parameters:
 #  name
-#   Name of the aggregate. Used to identify it.
+#   Name of the interface to use. Used to identify it.
 #  plugins
 #   Plugins to use for this resource. Check the pmacct docs for supported plugins.
 #
@@ -31,8 +31,15 @@ class gen_pmacct {
 define gen_pmacct::config ($aggregates, $plugins, $sql_host, $sql_db, $sql_user, $sql_passwd, $sql_history, $sql_history_roundoff, $sql_refresh_time, $sql_dont_try_update) {
   include gen_pmacct
 
+  $table_part = regsubst($hostname, '-', '_', 'G')
+
   file { "/etc/pmacct/pmacctd.${name}.conf":
     content => template("gen_pmacct/pmacct.conf"),
     notify  => Service["pmacct"],
+  }
+
+  exec { "/bin/sed -i 's/^\\(INTERFACES=\".*\\)\"$/\1 ${name}\"/' /etc/default/pmacct":
+    unless => "/bin/sh -c '. /etc/default/pmacct; for i in \$INTERFACES; do if test \$i = ${name}; then exit 0; fi; done; exit 1'",
+    notify => Service["pmacct"],
   }
 }
