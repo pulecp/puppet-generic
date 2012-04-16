698faf3037fab590e8e9c9e614361f7b401f3525
Tim Stoop <tim@kumina.nl>
2012-04-16 18:13:32 +0200

    Make em work on machines without postgresql (like, don't give errors).

--- gen_postgresql/lib/facter/psql_dbs.rb
+++ gen_postgresql/lib/facter/psql_dbs.rb
@@ -1,8 +1,10 @@
 begin
-  users = %x{cd /tmp; /usr/bin/sudo -u postgres /usr/bin/psql -A -F ';' -t -c '\\l' | /usr/bin/awk -F';' '{print $1}' | /bin/grep -v =}.split("\n").join(';')
-  Facter.add("psql_dbs") do
-    setcode do
-      users
+  if File.exist?("/etc/init.d/postgresql")
+    dbs = %x{cd /tmp; /usr/bin/sudo -u postgres /usr/bin/psql -A -F ';' -t -c '\\l' | /usr/bin/awk -F';' '{print $1}' | /bin/grep -v =}.split("\n").join(';')
+    Facter.add("psql_dbs") do
+      setcode do
+        dbs
+      end
     end
   end
 rescue
--- gen_postgresql/lib/facter/psql_users.rb
+++ gen_postgresql/lib/facter/psql_users.rb
@@ -1,8 +1,10 @@
 begin
-  users = %x{cd /tmp; /usr/bin/sudo -u postgres /usr/bin/psql -A -F ';' -t -c '\\du' | /usr/bin/awk -F ';' '{print $1}'}.split("\n").join(';')
-  Facter.add("psql_users") do
-    setcode do
-      users
+  if File.exist?("/etc/init.d/postgresql")
+    users = %x{cd /tmp; /usr/bin/sudo -u postgres /usr/bin/psql -A -F ';' -t -c '\\du' | /usr/bin/awk -F ';' '{print $1}'}.split("\n").join(';')
+    Facter.add("psql_users") do
+      setcode do
+        users
+      end
     end
   end
 rescue
