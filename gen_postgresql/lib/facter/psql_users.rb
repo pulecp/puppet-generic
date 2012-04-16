39ef934e3989938ee803f2a13e7091d6cb69296e
Tim Stoop <tim@kumina.nl>
2012-04-16 17:29:20 +0200

    Add simple facts for postgresql.

---
+++ gen_postgresql/lib/facter/psql_users.rb
@@ -0,0 +1,10 @@
+begin
+  users = %x{cd /tmp; /usr/bin/sudo -u postgres /usr/bin/psql -A -F ';' -t -c '\\du' | /usr/bin/awk -F ';' '{print $1}'}.split("\n").join(';')
+  Facter.add("psql_users") do
+    setcode do
+      users
+    end
+  end
+rescue
+# do nothing
+end
