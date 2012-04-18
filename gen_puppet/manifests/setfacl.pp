pulecp/puppet-generic

commit c9bbd35715093456fec7ae575f205de5c7820b35

Add ability to disable recursion.

gen_puppet/manifests/setfacl.pp
@@ -17,10 +17,15 @@
 #  Undocumented
 #  gen_puppet
 #
-define setfacl ($dir=false, $acl, $make_default = false) {
+define setfacl ($dir=false, $acl, $make_default = false, $recurse = true) {
   $real_dir = $dir ? {
     false   => $name,
     default => $dir,
+  }
+
+  $real_r = $recurse ? {
+    true    => "-R",
+    default => "",
   }
 
   if $make_default {
@@ -30,11 +35,11 @@ define setfacl ($dir=false, $acl, $make_default = false) {
 
     exec {
       "Set default acls '${acl}' on ${real_dir}":
-        command => "/usr/bin/setfacl -R -m default:${acl} ${real_dir}",
+        command => "/usr/bin/setfacl ${recurse} -m default:${acl} ${real_dir}",
         unless  => "/usr/bin/getfacl --absolute-names ${real_dir} | /bin/grep '^default:${acl}'",
         require => Kpackage["acl"];
       "Set default acls '${acl}' on ${real_dir} (mask)":
-        command => "/usr/bin/setfacl -R -m default:mask:rwx ${real_dir}",
+        command => "/usr/bin/setfacl ${recurse} -m default:mask:rwx ${real_dir}",
         unless  => "/usr/bin/getfacl --absolute-names ${real_dir} | /bin/grep '^default:mask::rwx'",
         require => Kpackage["acl"];
     }
@@ -42,11 +47,11 @@ define setfacl ($dir=false, $acl, $make_default = false) {
 
   exec {
     "Set acls '${acl}' on ${real_dir}":
-      command => "/usr/bin/setfacl -R -m ${acl} ${real_dir}",
+      command => "/usr/bin/setfacl ${recurse} -m ${acl} ${real_dir}",
       unless  => "/usr/bin/getfacl --absolute-names ${real_dir} | /bin/grep '^${acl}'",
       require => Kpackage["acl"];
     "Set acls '${acl}' on ${real_dir} (mask)":
-      command => "/usr/bin/setfacl -R -m mask:rwx ${real_dir}",
+      command => "/usr/bin/setfacl ${recurse} -m mask:rwx ${real_dir}",
       unless  => "/usr/bin/getfacl --absolute-names ${real_dir} | /bin/grep '^mask::rwx'",
       require => Kpackage["acl"];
   }

commit 2bc4302b794eebe23188c87f72519b677e8838da

Fix setfacl regression caused by c9bbd35715093456fec7ae575f205de5c7820b35.

gen_puppet/manifests/setfacl.pp
@@ -35,11 +35,11 @@ define setfacl ($dir=false, $acl, $make_default = false, $recurse = true) {
 
     exec {
       "Set default acls '${acl}' on ${real_dir}":
-        command => "/usr/bin/setfacl ${recurse} -m default:${acl} ${real_dir}",
+        command => "/usr/bin/setfacl ${real_r} -m default:${acl} ${real_dir}",
         unless  => "/usr/bin/getfacl --absolute-names ${real_dir} | /bin/grep '^default:${acl}'",
         require => Kpackage["acl"];
       "Set default acls '${acl}' on ${real_dir} (mask)":
-        command => "/usr/bin/setfacl ${recurse} -m default:mask:rwx ${real_dir}",
+        command => "/usr/bin/setfacl ${real_r} -m default:mask:rwx ${real_dir}",
         unless  => "/usr/bin/getfacl --absolute-names ${real_dir} | /bin/grep '^default:mask::rwx'",
         require => Kpackage["acl"];
     }
@@ -47,11 +47,11 @@ define setfacl ($dir=false, $acl, $make_default = false, $recurse = true) {
 
   exec {
     "Set acls '${acl}' on ${real_dir}":
-      command => "/usr/bin/setfacl ${recurse} -m ${acl} ${real_dir}",
+      command => "/usr/bin/setfacl ${real_r} -m ${acl} ${real_dir}",
       unless  => "/usr/bin/getfacl --absolute-names ${real_dir} | /bin/grep '^${acl}'",
       require => Kpackage["acl"];
     "Set acls '${acl}' on ${real_dir} (mask)":
-      command => "/usr/bin/setfacl ${recurse} -m mask:rwx ${real_dir}",
+      command => "/usr/bin/setfacl ${real_r} -m mask:rwx ${real_dir}",
       unless  => "/usr/bin/getfacl --absolute-names ${real_dir} | /bin/grep '^mask::rwx'",
       require => Kpackage["acl"];
   }