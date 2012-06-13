2f7279cf2328c0023e4db2c3edd00d3369e5def8
Rutger Spiertz <rutger@kumina.nl>
2012-06-13 14:51:27 +0200

    Add device_name option.

--- gen_drbd/manifests/init.pp
+++ gen_drbd/manifests/init.pp
@@ -17,7 +17,7 @@ class gen_drbd::common {
 }
 
 define gen_drbd($mastermaster=true, $time_out=false, $connect_int=false, $ping_int=false, $ping_timeout=false, $after_sb_0pri="discard-younger-primary",
-    $after_sb_1pri="discard-secondary", $after_sb_2pri="call-pri-lost-after-sb", $rate="5M", $verify_alg="md5", $use_ipaddress=$external_ipaddress) {
+    $after_sb_1pri="discard-secondary", $after_sb_2pri="call-pri-lost-after-sb", $rate="5M", $verify_alg="md5", $use_ipaddress=$external_ipaddress, $device_name=$name) {
   include gen_drbd::common
 
   if !defined(File["/etc/drbd.d/global_common.conf"]) {
@@ -28,23 +28,23 @@ define gen_drbd($mastermaster=true, $time_out=false, $connect_int=false, $ping_i
     }
   }
 
-  concat { "/etc/drbd.d/${name}.res":
+  concat { "/etc/drbd.d/${device_name}.res":
     require => Package["drbd8-utils"],
     notify  => Service["drbd"];
   }
 
-  if !defined(Concat::Add_content["0_${name}"]) {
-    concat::add_content { "0_${name}":
+  if !defined(Concat::Add_content["0_${device_name}"]) {
+    concat::add_content { "0_${device_name}":
       content => template("gen_drbd/resource_base"),
-      target  => "/etc/drbd.d/${name}.res";
+      target  => "/etc/drbd.d/${device_name}.res";
     }
   }
 
-  @@concat::add_content { "1_${name}_${fqdn}":
+  @@concat::add_content { "1_${device_name}_${fqdn}":
     content => template("gen_drbd/resource_address"),
-    target  => "/etc/drbd.d/${name}.res",
-    tag     => "drbd_${environment}_${name}";
+    target  => "/etc/drbd.d/${device_name}.res",
+    tag     => "drbd_${environment}_${device_name}";
   }
 
-  Concat::Add_content <<| tag == "drbd_${environment}_${name}" |>>
+  Concat::Add_content <<| tag == "drbd_${environment}_${device_name}" |>>
 }
