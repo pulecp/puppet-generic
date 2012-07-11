70b3d48c6bc4039316deab16f587d6496cbf4237
Pieter Lexis <pieter@kumina.nl>
2012-07-11 16:03:39 +0200

    Fix the upgrades_available script for ruby 1.9

--- gen_apt/lib/facter/upgrades_available.rb
+++ gen_apt/lib/facter/upgrades_available.rb
@@ -1,5 +1,5 @@
 if File.exists?('/usr/bin/apt-get')
   # What does this do?
   # It simulates an apt-get upgrade, takes the output and finds all the lines starting with Inst and shows the size of it.
-  Facter.add('upgrades_available') { setcode { %x{/usr/bin/apt-get -s -o Debug::NoLocking=true upgrade}.grep(/^Inst/).size } }
+  Facter.add('upgrades_available') { setcode { %x{/usr/bin/apt-get -s -o Debug::NoLocking=true upgrade}.scan(/^Inst/).size } }
 end
