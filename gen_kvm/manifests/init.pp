7fead23c90fb22e9e39fa63a93c2fa6e0967a40f
Tim Stoop <tim@kumina.nl>
2012-10-23 10:13:38 +0200

    Depend on libspice-server1, add some squeeze-isms.

--- gen_kvm/manifests/init.pp
+++ gen_kvm/manifests/init.pp
@@ -9,10 +9,19 @@
 #  gen_puppet
 #
 class gen_kvm {
+  include gen_base::libspice_server1
+
   # Against our policy we use a define from an other gen class here, but we'd have to do other icky stuff if we didn't
-  if $lsbdistcodename == 'lenny' {
+  if $lsbdistcodename == 'lenny' or $lsbdistcodename == 'squeeze' {
     gen_apt::preference { "qemu-kvm":; }
   }
 
-  package { "qemu-kvm":; }
+  # In squeeze, pin both.
+  if $lsbdistcodename == 'squeeze' {
+    gen_apt::preference { "kvm":; }
+  }
+
+  package { "qemu-kvm":
+    ensure => latest,
+  }
 }
