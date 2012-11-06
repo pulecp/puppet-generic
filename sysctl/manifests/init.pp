a032c90e57e87241ccd3262c94fc5ab323c58292
Tim Stoop <tim@kumina.nl>
2012-11-06 15:40:36 +0100

    Rewrite some.

--- sysctl/manifests/init.pp
+++ sysctl/manifests/init.pp
@@ -3,20 +3,36 @@
 # Class: sysctl
 #
 # Actions:
-#	Undocumented
+#  Setup sysctl exec.
 #
 # Depends:
-#	Undocumented
-#	gen_puppet
 #
 class sysctl {
   exec { "reload-sysctl":
     command     => "/sbin/sysctl -p /etc/sysctl.conf",
-    subscribe   => File["/etc/sysctl.conf"],
     refreshonly => true;
   }
+}
 
-  file { "/etc/sysctl.conf":
-    checksum => "md5",
+# Define: sysctl::setting
+#
+# Actions:
+#  Setup a sysctl parameter
+#
+# Parameters:
+#  name:  A general name or the parameter you'd like to set.
+#  param: The parameter you'd like to set. Defaults to $name
+#  value: The value you'd like to set the $param to with sysctl.
+#
+# Depends:
+#  sysctl
+#
+define sysctl::setting ($param = $name, $value) {
+  include sysctl
+
+  exec { "Set ${param} to ${value} for Sysctl::Setting['${name}']":
+    command => "/bin/echo '${param} = ${value}' >> '/etc/sysctl.conf'",
+    unless  => "/bin/grep -Fx '${param} = ${value}' /etc/sysctl.conf",
+    notify  => Exec["reload-sysctl"];
   }
 }
