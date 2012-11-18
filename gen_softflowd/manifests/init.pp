80ee79e1217d3cc4cf4dbf6e265ef5702c500d6b
Thomas Ronner <thomas@kumina.nl>
2012-11-18 23:32:05 +0100

    Create gen_softflowd.

---
+++ gen_softflowd/manifests/init.pp
@@ -0,0 +1,27 @@
+# Author: Kumina bv <support@kumina.nl>
+
+# Class: gen_softflowd
+#
+# Actions:
+#  Configure softflowd
+#
+# Parameters:
+#   interface  The network interface to listen on (default 'eth0')
+#   host       IP of NetFlow collector (default '127.0.0.1')
+#   port       UDP port of the collector (default '9995')
+#   version    Netflow version (default '9')
+#
+# Depends:
+#  gen_puppet
+#
+class gen_softflowd ($interface='eth0', $host='127.0.0.1', $port='9995', $version='9') {
+  $nf_collector = "${host}:${port}"
+
+  kservice { 'softflowd':; }
+
+  file { '/etc/default/softflowd':
+    content => template('gen_softflowd/default'),
+    require => Package['softflowd'],
+    notify  => Service['softflowd'];
+  }
+}
