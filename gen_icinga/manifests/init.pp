232d8bb93b8386f2a0439cd9240aeea5e2f1704b
Rutger Spiertz <rutger@kumina.nl>
2012-11-27 22:16:38 +0100

    Remove base_check_command and old templates.

--- gen_icinga/manifests/init.pp
+++ gen_icinga/manifests/init.pp
@@ -140,7 +140,7 @@ class gen_icinga::server {
 define gen_icinga::service($conf_dir="${environment}/${fqdn}", $use=false, $service_description=false, $servicegroups=false, $host_name=false, $address=false, $hostgroup_name=false, $initial_state=false, $active_checks_enabled=false,
     $passive_checks_enabled=false, $obsess_over_service=false, $check_freshness=false, $freshness_threshold=false, $notifications_enabled=false, $event_handler_enabled=false, $flap_detection_enabled=false, $process_perf_data=false,
     $retain_status_information=false, $retain_nonstatus_information=false, $notification_interval=false, $is_volatile=false, $check_period=false, $check_interval=false, $retry_interval=false, $notification_period=false,
-    $notification_options=false, $contact_groups=false, $contacts=false, $max_check_attempts=false, $check_command=false, $base_check_command=false, $arguments=false, $register=false, $ensure=present, $proxy=false) {
+    $notification_options=false, $contact_groups=false, $contacts=false, $max_check_attempts=false, $check_command=false, $arguments=false, $register=false, $ensure=present, $proxy=false) {
   # Empty define as the Icinga config is pulled directly from the DB
 }
 
@@ -196,10 +196,9 @@ define gen_icinga::service($conf_dir="${environment}/${fqdn}", $use=false, $serv
 # Depends:
 #  gen_puppet
 #
-define gen_icinga::host($conf_dir="${environment}/${fqdn}", $use=false, $hostgroups=false, $parents=false, $address=$ipaddress, $initial_state=false, $ensure=present,
-    $notifications_enabled=false, $event_handler_enabled=false, $flap_detection_enabled=false, $process_perf_data=false, $retain_status_information=false, $retain_nonstatus_information=false,
-    $check_command = false, $base_check_command=false, $check_interval=false, $retry_interval=false, $notification_period=false, $notification_interval=false, $contact_groups=false,
-    $contacts=false, $max_check_attempts=false, $register=false, $proxy=false) {
+define gen_icinga::host($conf_dir="${environment}/${fqdn}", $use=false, $hostgroups=false, $parents=false, $address=$ipaddress, $initial_state=false, $ensure=present, $notifications_enabled=false, $event_handler_enabled=false,
+    $flap_detection_enabled=false, $process_perf_data=false, $retain_status_information=false, $retain_nonstatus_information=false, $check_command=false, $check_interval=false, $retry_interval=false, $notification_period=false,
+    $notification_interval=false, $contact_groups=false, $contacts=false, $max_check_attempts=false, $register=false, $proxy=false) {
   # Empty define as the Icinga config is pulled directly from the DB
 }
 
