e85b8f387b618dac9d22cbc27177f2e10a0eb246
Thomas Ronner <thomas@kumina.nl>
2012-12-08 18:04:46 +0100

    Add maxlife and expint expiry parameters to gen_softflowd.

--- gen_softflowd/manifests/init.pp
+++ gen_softflowd/manifests/init.pp
@@ -10,11 +10,13 @@
 #   host       IP of NetFlow collector (default '127.0.0.1')
 #   port       UDP port of the collector (default '9995')
 #   version    Netflow version (default '9')
+#   maxlife    Max lifetime of a flow (default '5m' (= five minutes))
+#   expint     Flow expire interval, expint=0 means expire immediately (default '0')
 #
 # Depends:
 #  gen_puppet
 #
-class gen_softflowd ($interface='eth0', $host='127.0.0.1', $port='9995', $version='9') {
+class gen_softflowd ($interface='eth0', $host='127.0.0.1', $port='9995', $version='9', $maxlife='5m', $expint='0') {
   $nf_collector = "${host}:${port}"
 
   kservice { 'softflowd':; }
