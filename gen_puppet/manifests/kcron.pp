2a1ddf4846d28b44dad93c006d77556ddc930c3d
Rutger Spiertz <rutger@kumina.nl>
2012-12-17 09:33:42 +0100

    Add option to pass env vars to cron.

--- gen_puppet/manifests/kcron.pp
+++ gen_puppet/manifests/kcron.pp
@@ -1,36 +1,21 @@
 # Author: Kumina bv <support@kumina.nl>
 
-# Define: kcron
-#
+# Define:  kcron
+# Actions: Add an entry to the crontab of the system
 # Parameters:
-#  mailto
-#    The mail address notifications are sent to
-#  minute
-#    Minute interval field of crontab entry
-#  hour
-#    Hour interval field of crontab entry
-#  mday
-#    Day of month interval field of crontab entry
-#  month
-#    Month interval field of crontab entry
-#  wday
-#    Day of week interval field of crontab entry
-#  user
-#    Which user should be used to execute the command
-#  first_wday_of_month
-#    If true, only run on first wday of month
-#  pacemaker_resource
-#    In failover setups, make sure this resource is local before running the command
-#  command
-#    The command of the crontab entry
-#
-# Actions:
-#  Add an entry to the crontab of the system
-#
-# Depends:
-#  gen_puppet
-#
-define kcron($ensure="present", $mailto='root', $minute="*", $hour="*", $mday="*", $month="*", $wday="*", $user="root", $first_wday_of_month=false, $pacemaker_resource=false, $command) {
+#  mailto:              The mail address notifications are sent to
+#  minute:              Minute interval field of crontab entry
+#  hour:                Hour interval field of crontab entry
+#  mday:                Day of month interval field of crontab entry
+#  month:               Month interval field of crontab entry
+#  wday:                Day of week interval field of crontab entry
+#  user:                Which user should be used to execute the command
+#  first_wday_of_month: If true, only run on first wday of month
+#  pacemaker_resource:  In failover setups, make sure this resource is local before running the command
+#  command:             The command of the crontab entry
+#  environment:         (Array of) environment var that need to be set for the cron
+# Depends: gen_puppet
+define kcron($ensure="present", $mailto='root', $minute="*", $hour="*", $mday="*", $month="*", $wday="*", $user="root", $first_wday_of_month=false, $pacemaker_resource=false, $command, $environment=false) {
   # If the name contains an underscore or dot, cron won't use the file! So fail when that's the case.
   if $name =~ /\./ or $name =~ /_/ or $name =~ /\// or $name =~ / / {
     fail("Kcron names cannot contain dots, underscores, slashes or spaces. Resource: ${name}")
