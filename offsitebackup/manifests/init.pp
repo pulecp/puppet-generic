14432c722e5c208ef114d0dfe09bf4b92a645530
Thomas Ronner <thomas@kumina.nl>
2012-12-28 18:14:32 +0100

    Create offsitebackup::extraclient to create extra backup jobs.

--- offsitebackup/manifests/init.pp
+++ offsitebackup/manifests/init.pp
@@ -70,10 +70,7 @@ class offsitebackup::common {
 #
 class offsitebackup::client($backup_server, $backup_home="/backup/${environment}", $backup_user=$environment, $backup_remove_older_than="30B", $splay=28800) {
   include gen_base::backup-scripts
-
-  package { ["offsite-backup"]:
-    ensure => latest;
-  }
+  include offsitebackup::client::package
 
   Sshkey <<| title == $backup_server |>>
 
@@ -105,6 +102,73 @@ class offsitebackup::client($backup_server, $backup_home="/backup/${environment}
     ensure => symlink,
     target => "/usr/share/offsite-backup/prepare/filesystems",
     require => Package["offsite-backup"];
+  }
+}
+
+class offsitebackup::client::package {
+  package { ["offsite-backup"]:
+    ensure => latest;
+  }
+}
+
+# Define: offsitebackup::extraclient
+#
+# Actions:
+#  Configures an extra backup client, e.g. to backup a certain folder elsewhere
+#
+# Parameters:
+#  splay: number of second to use as max splay. Defaults to 28800 (8 hours).
+#  confprefix: prefix config files in /etc/backup with this (defaults to "${name}_")
+#  backupdir: directory on the server; defaults to "${fqdn}_${name}"
+#  sshpubkey: ssh public key to use (string)
+#  sshprivkey: ssh private key to use (file)
+#  prepare: directory with prepare scripts (optional)
+#  prepare_conf: directory with configuration for the prepare scripts (optional)
+#  finish: directory with finish scripts (optional)
+#  finish_conf: directory with configuration for the finish scripts (optional)
+#  includes: a list of patterns to include
+#  excludes: a list of patterns to exclude
+#
+# Depends:
+#  Undocumented
+#  gen_puppet
+#
+define offsitebackup::extraclient($backup_server, $backup_home="/backup/${environment}", $backup_user=$environment, $backup_remove_older_than="30B", $splay=28800, $confprefix="${name}_", $sshpubkey, $sshprivkey, $backupdir="${fqdn}_${name}", $prepare="/dev/null", $prepare_conf="/dev/null", $finish="/dev/null", $finish_conf="/dev/null", $includes, $excludes) {
+  include gen_base::backup-scripts
+  include offsitebackup::client::package
+
+  Sshkey <<| title == $backup_server |>>
+
+  @@offsitebackup::common::backupkey { $backupdir:
+    backupserver => $backup_server,
+    backuproot => $backup_home,
+    user => $backup_user,
+    key => $sshpubkey,
+    require => Package["offsite-backup"],
+  }
+
+  $backup_rm_older_than = $backup_remove_older_than ? {
+    undef => "30B",
+    default => $backup_remove_older_than,
+  }
+
+  file {
+    "/etc/backup/${confprefix}includes":
+      content => template("offsitebackup/client/includes"),
+      require => Package["offsite-backup"];
+    "/etc/backup/${confprefix}excludes":
+      content => template("offsitebackup/client/excludes"),
+      require => Package["offsite-backup"];
+    "/etc/backup/${confprefix}offsite-backup.conf":
+      content => template("offsitebackup/client/offsite-backup.conf"),
+      require => Package["offsite-backup"];
+    "/etc/backup/${confprefix}ssh_key.pub":
+      content => $sshpubkey,
+      require => Package["offsite-backup"];
+    "/etc/backup/${confprefix}ssh_key":
+      mode    => 0400,
+      source  => $sshprivkey,
+      require => Package["offsite-backup"];
   }
 }
 
