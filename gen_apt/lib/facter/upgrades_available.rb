7af1102db42caff4b3c3972689020e2d780f894d
Pieter Lexis <pieter@kumina.nl>
2013-01-10 10:09:34 +0100

    Create a fact that shows the packages that can be upgraded.

--- gen_apt/lib/facter/upgrades_available.rb
+++ gen_apt/lib/facter/upgrades_available.rb
@@ -1,5 +1,7 @@
 if File.exists?('/usr/bin/apt-get')
   # What does this do?
-  # It simulates an apt-get upgrade, takes the output and finds all the lines starting with Inst and shows the size of it.
-  Facter.add('upgrades_available') { setcode { %x{/usr/bin/apt-get -s -o Debug::NoLocking=true upgrade}.scan(/^Inst/).size } }
+  # It simulates an apt-get upgrade, takes the output and finds all the lines starting with Inst and gets the package name from it.
+  packages =  %x{/usr/bin/apt-get -s -o Debug::NoLocking=true upgrade}.scan(/^Inst ([^ ]*)/)
+  Facter.add('upgrades_available') { setcode { packages.size } }
+  Facter.add('upgrades_available_packages') { setcode { packages.join(',') } }
 end
