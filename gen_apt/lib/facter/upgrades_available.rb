26e4d822e5e8828a1f0ca96c99879d7194e22b28
Pieter Lexis <pieter@kumina.nl>
2013-01-23 15:35:09 +0100

    Add a upgraded_packages fact

--- gen_apt/lib/facter/upgrades_available.rb
+++ gen_apt/lib/facter/upgrades_available.rb
@@ -5,3 +5,21 @@ if File.exists?('/usr/bin/apt-get')
   Facter.add('upgrades_available') { setcode { packages.size } }
   Facter.add('upgrades_available_packages') { setcode { packages.join(',') } }
 end
+
+# And now for the upgraded packages
+pkgs = ''
+files = ['/var/log/dpkg.log','/var/log/dpkg.log.1']
+files.each do |file|
+  if File.exist?(file)
+    File.open(file) do |io|
+      io.each_line do |line|
+        pkg = line.chomp.scan(/(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) upgrade ([^ ]*) ([^ ]*) ([^ ]*)/).flatten
+        if ! pkg.empty?
+          pkgs += "#{pkg[1]}|#{pkg[0]}|#{pkg[2]}|#{pkg[3]};"
+        end
+      end
+    end
+  end
+end
+
+Facter.add(:upgraded_packages) { setcode { pkgs.chomp(';') } }
