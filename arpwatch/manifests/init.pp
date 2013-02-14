f72e916a417a25e509fcf56939083a1474651cd5
Tim Stoop <tim@kumina.nl>
2013-02-14 09:13:41 +0100

    Allow to disable arpwatch.

--- arpwatch/manifests/init.pp
+++ arpwatch/manifests/init.pp
@@ -23,3 +23,20 @@ class arpwatch {
     require => Package["arpwatch"];
   }
 }
+
+class arpwatch::disable {
+  include arpwatch
+
+  Package <| title == 'arpwatch' |> {
+    ensure => absent,
+  }
+
+  Service <| title == 'arpwatch' |> {
+    ensure => stopped,
+    enable => false,
+  }
+
+  File <| title == '/etc/default/arpwatch' |> {
+    ensure => absent,
+  }
+}
