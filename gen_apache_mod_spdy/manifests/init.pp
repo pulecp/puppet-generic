436d745a018b6ed1ea6e363a3e17b6bef1a22e22
Tim Stoop <tim@kumina.nl>
2013-03-28 19:00:36 +0100

    Add mod_spdy for Apache.

---
+++ gen_apache_mod_spdy/manifests/init.pp
@@ -0,0 +1,21 @@
+# Author: Kumina bv <support@kumina.nl>
+
+# Class: gen_apache_mod_spdy
+#
+# Actions:
+#  Install the mod_spdy package from Google for Apache. Installing will automatically enable it.
+#
+class gen_apache_mod_spdy {
+  gen_apt::source { 'mod_spdy'
+    uri => 'http://dl.google.com/linux/mod-spdy/deb/',
+    components => 'main';
+  }
+
+  gen_apt::key { '7FAC5991':
+    content => template('gen_apache_mod_spdy/google-key');
+  }
+
+  package { 'mod-spdy-beta':
+    ensure => latest,
+  }
+}
