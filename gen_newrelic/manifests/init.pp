f33551a2ccf9f906c4306cc67b256eba96b37691
Tim Stoop <tim@kumina.nl>
2013-04-10 11:54:13 +0200

    Add newrelic class.

---
+++ gen_newrelic/manifests/init.pp
@@ -0,0 +1,72 @@
+# Author: Kumina bv <support@kumina.nl>
+
+# Class: gen_newrelic
+#
+# Actions:
+#  Set up NewRelic.
+#
+# Parameters:
+#  key: The key for NewRelic.
+#
+# Depends:
+#  gen_apt
+#
+class gen_newrelic ($key) {
+  gen_apt::source { 'newrelic':
+    uri          => 'http://apt.newrelic.com/debian/',
+    distribution => 'newrelic',
+    components   => 'non-free';
+  }
+
+  gen_apt::key { '548C16BF':
+    content => template('gen_newrelic/newrelic-key');
+  }
+
+  package { 'newrelic-sysmond':
+    ensure => latest,
+  }
+
+  file { '/etc/newrelic/nrsysmond.cfg':
+    content => template('gen_newrelic/nrsysmond.cfg'),
+    require => Package['newrelic-sysmond'],
+  }
+
+  service { 'newrelic-sysmond':
+    require   => Package['newrelic-sysmond'],
+    ensure    => running,
+    hasstatus => true,
+  }
+}
+
+# Class: gen_newrelic::php
+#
+# Actions:
+#  Setup the PHP daemon for NewRelic.
+#
+# Depends:
+#  gen_newrelic
+#
+class gen_newrelic::php {
+  $key = $gen_newrelic::key
+
+  package {
+    'newrelic-daemon':
+      ensure => latest;
+    ['newrelic-php5','newrelic-php5-common']:
+      ensure => latest,
+      notify => Exec['reload-apache2'],
+      require => File['/var/log/newrelic/newrelic-daemon.log'];
+  }
+
+  file {
+    '/var/log/newrelic/newrelic-daemon.log':
+      owner   => 'newrelic',
+      group   => 'www-data',
+      mode    => 660,
+      require => Package['newrelic-daemon'];
+    '/etc/php5/conf.d/newrelic.ini':
+      content => template('gen_newrelic/php.ini'),
+      require => Package['newrelic-php5'],
+      notify  => Exec['reload-apache2'];
+  }
+}
