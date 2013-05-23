ff80dccba5b6f7ca7c7c0d57dd2c788ce63e9573
Tim Stoop <tim@kumina.nl>
2013-05-23 15:44:53 +0200

    Rewrite to new style.

--- gen_subversion/manifests/init.pp
+++ gen_subversion/manifests/init.pp
@@ -1,166 +1,75 @@
 # Author: Kumina bv <support@kumina.nl>
 
-# Class: subversion
+# Class: gen_subversion::server
 #
 # Actions:
-#  Undocumented
+#  Setup default stuff for a server hosting subversion repositories.
 #
-# Depends:
-#  Undocumented
-#  gen_puppet
-#
-class subversion {
-  package { "subversion":
-    ensure => installed,
-  }
+class gen_subversion::server {
+  include gen_subversion::client
 
   file { "/srv/svn":
-    owner => "root",
-    group => "root",
-    mode => 755,
+    owner  => "root",
+    group  => "root",
+    mode   => 755,
     ensure => directory,
-  }
-
-  # let's define a nice component for svn
-  define repo($group, $path=false, $svn_via_dav=false, $mode="2755") {
-    if $path {
-      $svndir = $path
-    } else {
-      $svndir = "/srv/svn/$name"
-    }
-
-    # Set the correct group
-    if $svn_via_dav {
-      debug('Determined SVN will be used through DAV, change svnowner to www-data.')
-      $svnowner = "www-data"
-    } else {
-      debug('Determined SVN will NOT be used through DAV, change svnowner to root.')
-      $svnowner = "root"
-    }
-
-    # create the repo
-    exec { "create-svn-$name":
-      command => "/usr/bin/svnadmin create --fs-type fsfs $svndir",
-      creates => "$svndir",
-    }
-
-    # Make sure the files and directories have the correct group
-    file { "$svndir":
-      require => Exec["create-svn-$name"],
-      mode => $mode,
-      owner => $svnowner,
-      group => $group,
-      recurse => false,
-    }
-
-    # Grant write permissions to the group where needed
-    file {
-      "$svndir/conf":
-        mode => 2775,
-        owner => $svnowner,
-        group => $group,
-        recurse => false;
-      "$svndir/db":
-        mode => 2775,
-        owner => $svnowner,
-        group => $group,
-        recurse => false;
-      "$svndir/db/current":
-        mode => 664,
-        owner => $svnowner,
-        group => $group;
-      "$svndir/db/revprops":
-        mode => 2775,
-        owner => $svnowner,
-        group => $group;
-      "$svndir/db/revprops/0":
-        mode => 664,
-        owner => $svnowner,
-        group => $group;
-      "$svndir/db/revs":
-        mode => 2775,
-        owner => $svnowner,
-        group => $group;
-      "$svndir/db/revs/0":
-        mode => 664,
-        owner => $svnowner,
-        group => $group;
-      "$svndir/db/transactions":
-        mode => 2775,
-        owner => $svnowner,
-        group => $group;
-      "$svndir/db/uuid":
-        mode => 664,
-        owner => $svnowner,
-        group => $group;
-      "$svndir/db/write-lock":
-        mode => 664,
-        owner => $svnowner,
-        group => $group;
-      "$svndir/locks":
-        mode => 2775,
-        owner => $svnowner,
-        group => $group,
-        recurse => false;
-      "$svndir/hooks":
-        mode => 775,
-        owner => $svnowner,
-        group => $group,
-        recurse => false;
-      "$svndir/hooks/post-commit.tmpl":
-        mode => 664,
-        owner => $svnowner,
-        group => $group;
-      "$svndir/hooks/post-lock.tmpl":
-        mode => 664,
-        owner => $svnowner,
-        group => $group;
-      "$svndir/hooks/post-revprop-change.tmpl":
-        mode => 664,
-        owner => $svnowner,
-        group => $group;
-      "$svndir/hooks/post-unlock.tmpl":
-        mode => 664,
-        owner => $svnowner,
-        group => $group;
-      "$svndir/hooks/pre-commit.tmpl":
-        mode => 664,
-        owner => $svnowner,
-        group => $group;
-      "$svndir/hooks/pre-lock.tmpl":
-        mode => 664,
-        owner => $svnowner,
-        group => $group;
-      "$svndir/hooks/pre-revprop-change.tmpl":
-        mode => 664,
-        owner => $svnowner,
-        group => $group;
-      "$svndir/hooks/pre-unlock.tmpl":
-        mode => 664,
-        owner => $svnowner,
-        group => $group;
-      "$svndir/hooks/start-commit.tmpl":
-        mode => 664,
-        owner => $svnowner,
-        group => $group;
-      "$svndir/locks/db-logs.lock":
-        group => $group,
-        owner => $svnowner,
-        mode => 664;
-      "$svndir/locks/db.lock":
-        group => $group,
-        owner => $svnowner,
-        mode => 664;
-      "$svndir/dav":
-        group => $group,
-        owner => $svnowner,
-        mode => 2775;
-    }
   }
 }
 
-class subversion::client {
+# Class: gen_subversion::client
+#
+# Actions: Install the subversion package.
+#
+class gen_subversion::client {
   package { "subversion":
     ensure => latest;
+  }
+}
+
+# Define: gen_subversion::repo
+#
+# Actions: Setup a subversion repository.
+#
+define gen_subversion::repo($group, $svndir="/srv/svn/${name}", $mode="2755", $svnowner='root') {
+  include gen_subversion::server
+
+  # create the repo
+  exec { "create-svn-${name}":
+    command => "/usr/bin/svnadmin create --fs-type fsfs ${svndir}",
+    creates => $svndir,
+    require => [File['/srv/svn'],Package['subversion']];
+  }
+
+  # Make sure the files and directories have the correct group
+  file { $svndir:
+    require => Exec["create-svn-${name}"],
+    mode    => $mode,
+    owner   => $svnowner,
+    group   => $group,
+    recurse => false,
+  }
+
+  # Grant write permissions to the group where needed
+  file {
+    ["${svndir}/conf","${svndir}/db","${svndir}/locks"]:
+      mode    => 2775,
+      owner   => $svnowner,
+      group   => $group,
+      recurse => false;
+    ["${svndir}/db/current","${svndir}/db/revprops/0","${svndir}/db/revs/0","${svndir}/db/uuid","${svndir}/db/write-lock","${svndir}/hooks/post-commit.tmpl",
+     "${svndir}/hooks/post-lock.tmpl","${svndir}/hooks/post-revprop-change.tmpl","${svndir}/hooks/post-unlock.tmpl","${svndir}/hooks/pre-commit.tmpl","${svndir}/hooks/pre-lock.tmpl",
+     "${svndir}/hooks/pre-revprop-change.tmpl","${svndir}/hooks/pre-unlock.tmpl","${svndir}/hooks/start-commit.tmpl","${svndir}/locks/db-logs.lock","${svndir}/locks/db.lock"]:
+      mode    => 664,
+      owner   => $svnowner,
+      group   => $group;
+    ["${svndir}/db/revprops","${svndir}/db/revs","${svndir}/db/transactions","${svndir}/dav"]:
+      mode    => 2775,
+      owner   => $svnowner,
+      group   => $group;
+    "${svndir}/hooks":
+      mode    => 775,
+      owner   => $svnowner,
+      group   => $group,
+      recurse => false;
   }
 }
