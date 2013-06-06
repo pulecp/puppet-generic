979102692dce5d7152020e2940ad05e56b369f99
Tim Stoop <tim@kumina.nl>
2013-06-06 08:46:19 +0200

    First part of the rewrite.

--- gen_trac/manifests/init.pp
+++ gen_trac/manifests/init.pp
@@ -1,177 +1,78 @@
 # Author: Kumina bv <support@kumina.nl>
 
-# Class: trac::squeeze
+# Class: gen_trac
 #
 # Actions:
-#  Undocumented
+#  Setup the trac software.
 #
-# Depends:
-#  Undocumented
-#  gen_puppet
-#
-class trac::squeeze {
-}
-
-# Class: trac::lenny
-#
-# Actions:
-#  Undocumented
-#
-# Depends:
-#  Undocumented
-#  gen_puppet
-#
-class trac::lenny {
-}
-
-# Class: trac::etch
-#
-# Actions:
-#  Undocumented
-#
-# Depends:
-#  Undocumented
-#  gen_puppet
-#
-class trac::etch {
-  # Move the shared trac configuration to /etc/trac/trac.ini, where it
-  # belongs.
-  file { "/usr/share/trac/conf/trac.ini":
-    ensure => "/etc/trac/trac.ini",
-    owner => "root",
-    group => "root",
-    mode => 644,
-    backup => false,
-    require => File["/etc/trac/trac.ini"],
-  }
-}
-
-# Class: trac
-#
-# Actions:
-#  Undocumented
-#
-# Depends:
-#  Undocumented
-#  gen_puppet
-#
-class trac($mail_relay=false) {
-  include "trac::$lsbdistcodename"
-
-  package { ["trac", "enscript", "sqlite3", "python-setuptools"]:
+class gen_trac($mail_relay=false) {
+  package { ["trac"]:
     ensure => installed,
   }
 
   # Shared Trac configuration
   file { "/etc/trac/trac.ini":
     content => template("trac/trac.ini"),
-    owner => "root",
-    group => "root",
-    mode => 644,
+    owner   => "root",
+    group   => "root",
+    mode    => 644,
     require => Package["trac"],
   }
 
   # Directory in which Trac can unpack any Python Eggs
   file { "/var/cache/trac":
-    owner => "www-data",
-    group => "www-data",
-    mode => "0775",
+    owner  => "www-data",
+    group  => "www-data",
+    mode   => "0775",
     ensure => "directory",
   }
 
   file { "/srv/trac":
     ensure => directory;
   }
+}
 
-  define environment($group, $path=false, $svnrepo=false) {
-    if $path {
-      $tracdir = $path
-    } else {
-      $tracdir = "/srv/trac/$name"
-    }
+define gen_trac::environment($group, $path=false, $svnrepo=false) {
+  include gen_trac
 
-    if $svnrepo {
-      $svndir = $svnrepo
-    } else {
-      $svndir = "/srv/svn/$name"
-    }
+  if $path {
+    $tracdir = $path
+  } else {
+    $tracdir = "/srv/trac/$name"
+  }
 
-    # Create the Trac environment
-    exec { "create-trac-$name":
-      command => $lsbdistcodename ? {
-        "etch" => "/usr/bin/trac-admin $tracdir initenv $name sqlite:db/trac.db svn $svndir /usr/share/trac/templates",
-        "lenny" => "/usr/bin/trac-admin $tracdir initenv $name sqlite:db/trac.db svn $svndir",
-      },
-      logoutput => false,
-      creates => "$tracdir/conf/trac.ini",
-      require => Package["trac"],
-    }
+  if $svnrepo {
+    $svndir = $svnrepo
+  } else {
+    $svndir = "/srv/svn/$name"
+  }
 
-    # www-data needs read and write access to the trac database,
-    # log files, and configuration file (for WebAdmin)
-    file {
-      "$tracdir":
-        owner => "www-data",
-        group => $group,
-        recurse => false,
-        mode => 0750,
-        require => Exec["create-trac-$name"];
-      "$tracdir/db":
-        owner => "www-data",
-        group => $group,
-        recurse => false,
-        mode => 0775,
-        require => Exec["create-trac-$name"];
-      "$tracdir/db/trac.db":
-        owner => "www-data",
-        group => $group,
-        mode => 0664,
-        require => Exec["create-trac-$name"];
-      "$tracdir/log":
-        owner => "www-data",
-        group => $group,
-        mode => 0775,
-        require => Exec["create-trac-$name"];
-      "$tracdir/log/trac.log":
-        owner => "www-data",
-        group => $group,
-        mode => 0664,
-        require => Exec["create-trac-$name"];
-      "$tracdir/conf":
-        owner => "www-data",
-        group => $group,
-        mode => 0775,
-        require => Exec["create-trac-$name"];
-      "$tracdir/conf/trac.ini":
-        owner => "www-data",
-        group => $group,
-        mode => 0664,
-        require => Exec["create-trac-$name"];
-      "$tracdir/attachments":
-        owner => "www-data",
-        group => $group,
-        mode => 0775,
-        require => Exec["create-trac-$name"];
-      "$tracdir/templates":
-        owner => "root",
-        group => $group,
-        mode => 0775,
-        require => Exec["create-trac-$name"];
-      "$tracdir/wiki-macros":
-        owner => "root",
-        group => $group,
-        mode => 0775,
-        require => Exec["create-trac-$name"];
-      "$tracdir/htdocs":
-        owner => "root",
-        group => $group,
-        mode => 0775,
-        require => Exec["create-trac-$name"];
-      "$tracdir/plugins":
-        owner => "www-data",
-        group => $group,
-        mode => 0775,
-        require => Exec["create-trac-$name"];
-    }
+  # Create the Trac environment
+  exec { "create-trac-${name}":
+    command   => "/usr/bin/trac-admin ${tracdir} initenv ${name} sqlite:db/trac.db svn ${svndir}",
+    logoutput => false,
+    creates   => "${tracdir}/conf/trac.ini",
+    require   => Package["trac"],
+  }
+
+  # www-data needs read and write access to the trac database,
+  # log files, and configuration file (for WebAdmin)
+  file {
+    ["${tracdir}","${tracdir}/db"]:
+      owner   => "www-data",
+      group   => $group,
+      recurse => false,
+      mode    => 0750,
+      require => Exec["create-trac-${name}"];
+    ["${tracdir}/db/trac.db","${tracdir}/log/trac.log","${tracdir}/conf/trac.ini"]:
+      owner   => "www-data",
+      group   => $group,
+      mode    => 0664,
+      require => Exec["create-trac-${name}"];
+    ["${tracdir}/log","${tracdir}/conf","${tracdir}/attachments","${tracdir}/templates","${tracdir}/wiki-macros","${tracdir}/htdocs","${tracdir}/plugins"]:
+      owner   => "www-data",
+      group   => $group,
+      mode    => 0775,
+      require => Exec["create-trac-${name}"];
   }
 }
