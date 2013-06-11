864912894b0ffb20ef983c9aacf3a7dd021c4758
Tim Stoop <tim@kumina.nl>
2013-06-11 12:40:26 +0200

    Allow git repo and do not set global trac.init on non-squeeze.

--- gen_trac/manifests/init.pp
+++ gen_trac/manifests/init.pp
@@ -10,13 +10,15 @@ class gen_trac($mail_relay=false) {
     ensure => installed,
   }
 
-  # Shared Trac configuration
-  file { "/etc/trac/trac.ini":
-    content => template("gen_trac/trac.ini"),
-    owner   => "root",
-    group   => "root",
-    mode    => 644,
-    require => Package["trac"],
+  if $lsbdistcodename == 'squeeze' {
+    # Shared Trac configuration
+    file { "/etc/trac/trac.ini":
+      content => template("gen_trac/trac.ini"),
+      owner   => "root",
+      group   => "root",
+      mode    => 644,
+      require => Package["trac"],
+    }
   }
 
   # Directory in which Trac can unpack any Python Eggs
@@ -32,7 +34,27 @@ class gen_trac($mail_relay=false) {
   }
 }
 
-define gen_trac::environment($group, $path=false, $svnrepo=false) {
+# Class: gen_trac::git
+#
+# Actions: Make sure that trac can talk to git
+#
+class gen_trac::git {
+  package { 'trac-git':
+    ensure => latest;
+  }
+}
+
+# Define: gen_trac::environment
+#
+# Actions: Setup a Trac environment for a project
+#
+# Parameters:
+#  group: The group which should have access to this Trac instance, mostly for writing and changing files
+#  path: A specific path to use. Defaults to '/srv/trac/$name'.
+#  svnrepo: The subversion repository to use for this. Defaults to false, which means 'don't use a svn repo'.
+#  gitrepo: Like svnrepo, but for git.
+#
+define gen_trac::environment($group, $path="/srv/trac/${name}", $svnrepo=false, $gitrepo=false) {
   include gen_trac
 
   if $path {
@@ -42,14 +64,17 @@ define gen_trac::environment($group, $path=false, $svnrepo=false) {
   }
 
   if $svnrepo {
-    $svndir = $svnrepo
+    $repostring = "svn ${svnrepo}"
+  } elsif $gitrepo {
+    include gen_trac::git
+    $repostring = "git ${gitrepo}"
   } else {
-    $svndir = "/srv/svn/$name"
+    fail('Either a svnrepo or a gitrepo need to be set.')
   }
 
   # Create the Trac environment
   exec { "create-trac-${name}":
-    command   => "/usr/bin/trac-admin ${tracdir} initenv ${name} sqlite:db/trac.db svn ${svndir}",
+    command   => "/usr/bin/trac-admin ${tracdir} initenv ${name} sqlite:db/trac.db ${repostring}",
     logoutput => false,
     creates   => "${tracdir}/conf/trac.ini",
     require   => Package["trac"],
