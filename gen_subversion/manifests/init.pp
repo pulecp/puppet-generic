7ddc66182271c9f1b8fb7214833f913ca094381e
Tim Stoop <tim@kumina.nl>
2013-08-06 12:04:15 +0200

    Create new directories.

--- gen_subversion/manifests/init.pp
+++ gen_subversion/manifests/init.pp
@@ -63,7 +63,7 @@ define gen_subversion::repo($group, $svndir="/srv/svn/${name}", $mode="2755", $s
       mode    => 664,
       owner   => $svnowner,
       group   => $group;
-    ["${svndir}/db/revprops","${svndir}/db/revs","${svndir}/db/transactions","${svndir}/dav","${svndir}/db/txn-protorevs"]:
+    ["${svndir}/db/revprops","${svndir}/db/revs","${svndir}/db/transactions","${svndir}/dav","${svndir}/db/txn-protorevs","${svndir}/dav","${svndir}/dav/activities.d"]:
       mode    => 2775,
       owner   => $svnowner,
       group   => $group;
