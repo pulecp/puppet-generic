ca457d81c5d321ef3d51d527b5a3d8ac5b7a0161
Rutger Spiertz <rutger@kumina.nl>
2013-12-24 22:32:57 +0100

    Add gen_icinga2.

---
+++ gen_icinga2/manifests/init.pp
@@ -0,0 +1,26 @@
+class gen_icinga2::server {
+  include gen_icinga2::common
+
+  kservice { 'icinga2':; }
+}
+
+class gen_icinga2::classicui {
+  include gen_icinga2::common
+
+  package { 'icinga2-classicui':; }
+}
+
+class gen_icinga2::common {
+  gen_apt::key { '34410682':
+    content => template('gen_icinga2/34410682');
+  }
+
+  gen_apt::source { 'icinga2':
+    comment      => 'Icinga2 repository.',
+    sourcetype   => 'deb',
+    uri          => 'http://packages.icinga.org/debian',
+    distribution => 'icinga-wheezy',
+    components   => 'main',
+    key          => '34410682';
+  }
+}
