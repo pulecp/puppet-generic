8f8f7e0ee6faea0cc5f90cecf3b944699fb3ab3e
Pieter Lexis <pieter@kumina.nl>
2014-01-15 10:57:36 +0100

    gen_collectd: use our collectd packages with patches

--- gen_collectd/manifests/init.pp
+++ gen_collectd/manifests/init.pp
@@ -5,6 +5,13 @@ class gen_collectd {
       gen_apt::preference { ['collectd','collectd-core','collectd-dbg', 'collectd-dev',
                             'collectd-utils', 'libcollectdclient-dev', 'libcollectdclient0']:;
       }
+    if $lsbdistcodename == 'wheezy' {
+      # Pin to the kumina repo, these packages contain the patches for
+      # http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=735173
+      gen_apt::preference { ['collectd','collectd-core','collectd-dbg', 'collectd-dev',
+                            'collectd-utils', 'libcollectdclient-dev', 'libcollectdclient0']:
+        repo => 'kumina-wheezy';
+      }
     }
 
     kservice { 'collectd':
